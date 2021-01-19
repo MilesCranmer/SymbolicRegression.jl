2122784acd8330bd5e3f38eb5ea44966a3fef110
MilesCranmer <miles.cranmer@gmail.com>
2021-01-18 23:06:39 -0500

    Remove unused files

--- src/runfile.jl
+++
@@ -1,5 +0,0 @@
-@everywhere include("hyperparams.jl")
-@everywhere include("dataset.jl")
-@everywhere include("sr.jl")
-# fullRun(5, npop=1000, ncyclesperiteration=1000, fractionReplaced=0.100000f0, verbosity=round(Int32, 1000000000.000000), topn=10)
-# rmprocs(nprocs)
