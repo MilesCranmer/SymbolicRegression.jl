659a09906c9dc209822fa203551d81a0e25cef5c
MilesCranmer <miles.cranmer@gmail.com>
2021-01-27 21:57:34 -0500

    Working custom simplification rules

--- interface.jl
+++ interface.jl
@@ -1,39 +1,83 @@
 using SymbolicUtils
-# using Random
-# include("src/SymbolicRegression.jl")
-# using .SymbolicRegression: Node, Options, Population, printTree
-
-# Random.seed!(0)
-# options = Options(
-    # binary_operators=(+, *, /, -),
-    # unary_operators=(cos, exp),
-    # npopulations=4,
-# )
+using SymbolicUtils: Chain, If, RestartedChain, IfElse, Postwalk, Fixpoint, @ordered_acrule, isnotflat, flatten_term, needs_sorting, sort_args, is_literal_number, hasrepeats, merge_repeats, _isone, _iszero, _isinteger, istree, symtype, is_operation, has_trig
 
 mutable struct Node
     #Holds operators, variables, constants in a tree
     degree::Integer #0 for constant/variable, 1 for cos/sin, 2 for +/* etc.
-    val::Union{Float32, Integer} #Either const value, or enumerates variable
+    val::Union{Float32, Integer, Nothing} #Either const value, or enumerates variable
     constant::Bool #false if variable
     op::Integer #enumerates operator (separately for degree=1,2)
     l::Union{Node, Nothing}
     r::Union{Node, Nothing}
 
     Node(val::Float32) = new(0, val, true, 1, nothing, nothing)
+    Node(val::Float64) = new(0, convert(Float32, val), true, 1, nothing, nothing)
     Node(val::Integer) = new(0, val, false, 1, nothing, nothing)
-    Node(op::Integer, l::Node) = new(1, convert(Float32, 0), false, op, l, nothing)
-    Node(op::Integer, l::Union{Float32, Integer}) = new(1, convert(Float32, 0), false, op, Node(l), nothing)
-    Node(op::Integer, l::Node, r::Node) = new(2, convert(Float32, 0), false, op, l, r)
+    Node(val::String) = Node(parse(Int, val[2:end])) #e.g., "x0"
+    Node(op::Integer, l::Node) = new(1, nothing, false, op, l, nothing)
+    Node(op::Integer, l::Union{Float32, Integer}) = new(1, nothing, false, op, Node(l), nothing)
+    Node(op::Integer, l::Node, r::Node) = new(2, nothing, false, op, l, r)
+
+    Node(op::Integer, l::Union{Float32, Integer}, r::Node) = new(2, nothing, false, op, Node(l), r)
+    Node(op::Integer, l::Node, r::Union{Float32, Integer}) = new(2, nothing, false, op, l, Node(r))
+    Node(op::Integer, l::Union{Float32, Integer}, r::Union{Float32, Integer}) = new(2, nothing, false, op, Node(l), Node(r))
 end
+
+
+countNodes(tree::Nothing) = 0
+countNodes(tree::Node) = 1 + countNodes(tree.l) + countNodes(tree.r)
 
 #User-defined operations
 binops = (+, *, /, -)
 unaops = (cos, exp)
 
 
+function stringOp(op::F, tree::Node;
+                  bracketed::Bool=false,
+                  varMap::Union{Array{String, 1}, Nothing}=nothing)::String where {F}
+    if op in [+, -, *, /, ^]
+        l = stringTree(tree.l,  bracketed=false, varMap=varMap)
+        r = stringTree(tree.r,  bracketed=false, varMap=varMap)
+        if bracketed
+            return "$l $(string(op)) $r"
+        else
+            return "($l $(string(op)) $r)"
+        end
+    else
+        l = stringTree(tree.l,  bracketed=true, varMap=varMap)
+        r = stringTree(tree.r,  bracketed=true, varMap=varMap)
+        return "$(string(op))($l, $r)"
+    end
+end
+
+# Convert an equation to a string
+function stringTree(tree::Node;
+                    bracketed::Bool=false,
+                    varMap::Union{Array{String, 1}, Nothing}=nothing)::String
+    if tree.degree == 0
+        if tree.constant
+            return string(tree.val)
+        else
+            if varMap == nothing
+                return "x$(tree.val)"
+            else
+                return varMap[tree.val]
+            end
+        end
+    elseif tree.degree == 1
+        return "$(unaops[tree.op])($(stringTree(tree.l, bracketed=true, varMap=varMap)))"
+    else
+        return stringOp(binops[tree.op], tree, bracketed=bracketed, varMap=varMap)
+    end
+end
+
+# Print an equation
+function printTree(tree::Node; varMap::Union{Array{String, 1}, Nothing}=nothing)
+    println(stringTree(tree, varMap=varMap))
+end
+
 SymbolicUtils.istree(x::Node)::Bool = (x.degree > 0)
 SymbolicUtils.operation(x::Node)::Function = x.degree == 1 ? unaops[x.op] : binops[x.op]
-# SymbolicUtils.operation(x::Node)::Function = x.degree == 1 ? options.unaops[x.op] : options.binops[x.op]
 SymbolicUtils.arguments(x::Node)::Array{Node} = x.degree == 1 ? [x.l] : [x.l, x.r]
 SymbolicUtils.similarterm(x::Node, f, args) = begin
     nargs = length(args)
@@ -42,9 +86,11 @@ SymbolicUtils.similarterm(x::Node, f, args) = begin
     elseif nargs == 2
         f(args[1], args[2])
     else
-        f(args[1], similarterm(x, f, args[2:end]))
+        f(args[1], SymbolicUtils.similarterm(x, f, args[2:end]))
     end
 end
+SymbolicUtils.symtype(x::Node) = Real
+SymbolicUtils.promote_symtype(f, arg_symtypes...) = Real
 
 Base.hash(x::Node) = begin
     if x.degree == 0
@@ -55,6 +101,7 @@ Base.hash(x::Node) = begin
         hash(hash(x.op), hash(Base.hash(x.l), Base.hash(x.r)))
     end
 end
+
 Base.isequal(x::Node, y::Node)::Bool = begin
     if x.degree != y.degree
         false
@@ -68,45 +115,161 @@ Base.isequal(x::Node, y::Node)::Bool = begin
 end
 
 
-# for (op, f) in enumerate(map(Symbol, options.binops))
+## How SymbolicUtils orders:
+
+# <ₑ(a::Real,    b::Real) = abs(a) < abs(b)
+# <ₑ(a::Complex, b::Complex) = (abs(real(a)), abs(imag(a))) < (abs(real(b)), abs(imag(b)))
+# <ₑ(a::Real,    b::Complex) = true
+# <ₑ(a::Complex, b::Real) = false
+# <ₑ(a::Symbolic, b::Number) = false
+# <ₑ(a::Number,   b::Symbolic) = true
+# arglength(a) = length(arguments(a))
+# function <ₑ(a, b)
+    # if !istree(a) && !istree(b)
+        # T = typeof(a)
+        # S = typeof(b)
+        # return T===S ? isless(a, b) : nameof(T) < nameof(S)
+    # elseif istree(b) && !istree(a)
+        # return true
+    # elseif istree(a) && istree(b)
+        # return cmp_term_term(a,b)
+    # else
+        # return !(b <ₑ a)
+    # end
+# end
+
+# Enforce the order [+,-,\,/,^,*]
+
+Base.isless(x::Node, y::Node)::Bool = begin
+    nx = countNodes(x)
+    ny = countNodes(y)
+    if nx < ny
+        true
+    elseif nx > ny
+        false
+    elseif x.constant && y.constant
+        x.val < y.val
+    elseif x.constant && ~y.constant
+        true
+    elseif ~x.constant && y.constant
+        false
+    #Both are non-constant trees of equal size
+    elseif x.degree != y.degree
+        x.degree < y.degree
+    else
+        Base.hash(x) < Base.hash(y)
+    end
+end
+
 for (op, f) in enumerate(map(Symbol, binops))
     @eval begin
         Base.$f(l::Node, r::Node) = (l.constant && r.constant) ?  Node($f(l.val, r.val)) : Node($op, l, r)
+        Base.$f(l::Node, r::Real) = l.constant ?  Node($f(l.val, r)) : Node($op, l, convert(Float32, r))
+        Base.$f(l::Real, r::Node) = r.constant ?  Node($f(l, r.val)) : Node($op, convert(Float32, l), r)
     end
 end
 
-# for (op, f) in enumerate(map(Symbol, options.unaops))
 for (op, f) in enumerate(map(Symbol, unaops))
     @eval begin
-        Base.$f(l::Node) = l.constant ?  Node($f(l.val)) : Node($op, l)
+        Base.$f(l::Node) = l.constant ? Node($f(l.val)) : Node($op, l)
+        Base.$f(l::Real) = Node($f(l))
     end
 end
 
+PLUS_RULES = [
+    @rule(~x::isnotflat(+) => flatten_term(+, ~x))
+    @rule(~x::needs_sorting(+) => sort_args(+, ~x))
+    @ordered_acrule(~a::is_literal_number + ~b::is_literal_number => ~a + ~b)
 
-t = Node(1f0) + Node(1) + Node(1f0)
+    @acrule(*(~~x) + *(~β, ~~x) => *(1 + ~β, (~~x)...))
+    @acrule(*(~α, ~~x) + *(~β, ~~x) => *(~α + ~β, (~~x)...))
+    @acrule(*(~~x, ~α) + *(~~x, ~β) => *(~α + ~β, (~~x)...))
 
-t = SymbolicUtils.simplify(t)
+    @acrule(~x + *(~β, ~x) => *(1 + ~β, ~x))
+    @acrule(*(~α::is_literal_number, ~x) + ~x => *(~α + 1, ~x))
+    @rule(+(~~x::hasrepeats) => +(merge_repeats(*, ~~x)...))
 
-println(isequal(Node(1f0), Node(1f0)))
-println(t)
+    @ordered_acrule((~z::_iszero + ~x) => ~x)
+    @rule(+(~x) => ~x)
+]
+
+TIMES_RULES = [
+    @rule(~x::isnotflat(*) => flatten_term(*, ~x))
+    @rule(~x::needs_sorting(*) => sort_args(*, ~x))
+
+    @ordered_acrule(~a::is_literal_number * ~b::is_literal_number => ~a * ~b)
+    @rule(*(~~x::hasrepeats) => *(merge_repeats(^, ~~x)...))
+
+    @acrule((~y)^(~n) * ~y => (~y)^(~n+1))
+    @ordered_acrule((~x)^(~n) * (~x)^(~m) => (~x)^(~n + ~m))
+
+    @ordered_acrule((~z::_isone  * ~x) => ~x)
+    @ordered_acrule((~z::_iszero *  ~x) => ~z)
+    @rule(*(~x) => ~x)
+]
 
 
+POW_RULES = [
+    @rule(^(*(~~x), ~y::_isinteger) => *(map(a->pow(a, ~y), ~~x)...))
+    @rule((((~x)^(~p::_isinteger))^(~q::_isinteger)) => (~x)^((~p)*(~q)))
+    @rule(^(~x, ~z::_iszero) => 1)
+    @rule(^(~x, ~z::_isone) => ~x)
+    @rule(inv(~x) => ~x ^ -1)
+]
 
+ASSORTED_RULES = [
+    @rule(identity(~x) => ~x)
+    @rule(-(~x) => -1*~x)
+    @rule(-(~x, ~y) => ~x + -1(~y))
+    @rule(~x::_isone \ ~y => ~y)
+    @rule(~x \ ~y => ~y / (~x))
+    @rule(~x / ~y => ~x * pow(~y, -1))
+    @rule(one(~x) => one(symtype(~x)))
+    @rule(zero(~x) => zero(symtype(~x)))
+    @rule(cond(~x::is_literal_number, ~y, ~z) => ~x ? ~y : ~z)
+]
 
+TRIG_RULES = [
+    @acrule(sin(~x)^2 + cos(~x)^2 => one(~x))
+    @acrule(sin(~x)^2 + -1        => cos(~x)^2)
+    @acrule(cos(~x)^2 + -1        => sin(~x)^2)
 
+    @acrule(tan(~x)^2 + -1*sec(~x)^2 => one(~x))
+    @acrule(tan(~x)^2 +  1 => sec(~x)^2)
+    @acrule(sec(~x)^2 + -1 => tan(~x)^2)
 
+    @acrule(cot(~x)^2 + -1*csc(~x)^2 => one(~x))
+    @acrule(cot(~x)^2 +  1 => csc(~x)^2)
+    @acrule(csc(~x)^2 + -1 => cot(~x)^2)
+]
 
+function number_simplifier()
+    rule_tree = [If(istree, Chain(ASSORTED_RULES)),
+                 If(is_operation(+),
+                    Chain(PLUS_RULES)),
+                 If(is_operation(*),
+                    Chain(TIMES_RULES)),
+                 If(is_operation(^),
+                    Chain(POW_RULES))] |> RestartedChain
 
+    rule_tree
+end
 
+trig_simplifier(;kw...) = Chain(TRIG_RULES)
 
-# X = randn(Float32, 100, 5)
-# y = 2 * cos.(X[:, 4]) + X[:, 1] .^ 2 .- 2
-# pop = Population(X, y, 1f0, npop=10, nlength=3, options=options, nfeatures=5)
-# t = pop.members[1].tree
-# printTree(t, options)
-# println(SymbolicUtils.arguments((t, options)))
+function default_simplifier(; kw...)
+    IfElse(has_trig,
+           Postwalk(Chain((number_simplifier(),
+                           trig_simplifier())),
+                    ; kw...),
+           Postwalk(number_simplifier())
+                    ; kw...)
+end
 
+# reduce overhead of simplify by defining these as constant
+serial_simplifier = If(istree, Fixpoint(default_simplifier()))
 
-
+threaded_simplifier(cutoff) = Fixpoint(default_simplifier(threaded=true,
+                                                          thread_cutoff=cutoff))
 
 
