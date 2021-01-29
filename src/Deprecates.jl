4c0ca0de42db5219b7ec6abf7e4c7b8553cc7223
MilesCranmer <miles.cranmer@gmail.com>
2021-01-29 08:29:41 -0500

    Use transpose, not adjoint

--- src/Deprecates.jl
+++ src/Deprecates.jl
@@ -1,4 +1,4 @@
 using Base: @deprecate
 
 # Now the batch dimension is the last axis!
-@deprecate RunSR(X, y; kw...) EquationSearch(convert(Array, X'), y; kw...)
+@deprecate RunSR(X, y; kw...) EquationSearch(copy(transpose(X)), y; kw...)
