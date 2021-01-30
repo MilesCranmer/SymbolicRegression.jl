9ef5d69126a343faeabcea9ee26bedf45cbea1ed
MilesCranmer <miles.cranmer@gmail.com>
2021-01-30 05:05:41 -0500

    Update example

--- example.jl
+++ example.jl
@@ -1,20 +1,11 @@
-using Distributed
+include("src/SymbolicRegression.jl")
+using .SymbolicRegression
 using SymbolicUtils
-# using ClusterManagers
-
-# Easy to set up distributed over cores:
-# procs1 = addprocs(SlurmManager(4))
-# procs2 = addprocs(40) # 40 cores per node
-procs = addprocs()
-
-@everywhere include("src/SymbolicRegression.jl")
-@everywhere using .SymbolicRegression
 
 X = randn(Float32, 5, 100)
 y = 2 * cos.(X[4, :]) + X[1, :] .^ 2 .- 2
 
-
-@everywhere inv(x)=1/x
+inv(x) = 1/x
 
 options = SymbolicRegression.Options(
     binary_operators=(+, *), #, /, -),
@@ -29,8 +20,3 @@ dominating = calculateParetoFrontier(X, y, hallOfFame, options)
 eqn = node_to_symbolic(dominating[end].tree, options, evaluate_functions=true)
 
 print(simplify(eqn*5 + 3))
-# hallOfFame = RunSR(transpose(X), y, niterations=niterations, options=options)
-
-rmprocs(procs)
-# rmprocs(procs1)
-# rmprocs(procs2)
