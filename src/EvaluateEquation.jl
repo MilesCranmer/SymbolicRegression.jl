b83ae84b4c259a6df26a03258e804539dff128e9
MilesCranmer <miles.cranmer@gmail.com>
2021-02-04 00:13:26 -0500

    Smarter fusing of equations; seems to speed up

--- src/EvaluateEquation.jl
+++ src/EvaluateEquation.jl
@@ -1,6 +1,6 @@
 # Evaluate an equation over an array of datapoints
-# This one is just for reference.
-function evalTreeArray(tree::Node, cX::AbstractMatrix{T}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real}
+# This one is just for reference. The fused one should be faster.
+function unfusedEvalTreeArray(tree::Node, cX::AbstractMatrix{T}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real}
     if tree.degree == 0
         deg0_eval(tree, cX, options)
     elseif tree.degree == 1
@@ -10,48 +10,160 @@ function evalTreeArray(tree::Node, cX::AbstractMatrix{T}, options::Options)::Tup
     end
 end
 
-function check_array(cumulator::AbstractVector{T})::Bool where {T<:Real}
-    @inbounds @simd for i=1:size(cumulator, 1)
-        if (cumulator[i] != cumulator[i])::Bool || (cumulator[i] - cumulator[i] != convert(T, 0))::Bool
-            return false
+# Fuse doublets and triplets of operations for speed:
+function evalTreeArray(tree::Node, cX::AbstractMatrix{T}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real}
+    if tree.degree == 0
+        deg0_eval(tree, cX, options)
+    elseif tree.degree == 1
+        if tree.l.degree == 1
+            deg1_l1_eval(tree, cX, Val(tree.op), Val(tree.l.op), options)
+        else
+            deg1_eval(tree, cX, Val(tree.op), options)
+        end
+    else
+        if tree.l.degree == 1 && tree.r.degree == 1
+            deg2_l1_r1_eval(tree, cX, Val(tree.op), Val(tree.l.op), Val(tree.r.op), options)
+        elseif tree.l.degree == 1
+            deg2_l1_eval(tree, cX, Val(tree.op), Val(tree.l.op), options)
+        elseif tree.r.degree == 1
+            deg2_r1_eval(tree, cX, Val(tree.op), Val(tree.r.op), options)
+        else
+            deg2_eval(tree, cX, Val(tree.op), options)
         end
     end
-    return true
+end
+
+macro break_on_check(val, flag, type)
+    :(if ($(esc(val)) != $(esc(val)))::Bool || ($(esc(val)) - $(esc(val)) != convert($(esc(type)), 0))::Bool
+          $(esc(flag)) = false
+          break
+    end)
+end
+
+macro return_on_false(flag, n, type)
+    :(if !$(esc(flag))
+        return (Array{$(esc(type)), 1}(undef, $(esc(n))), false)
+    end)
 end
 
 function deg2_eval(tree::Node, cX::AbstractMatrix{T}, ::Val{op_idx}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real,op_idx}
-     (cumulator, complete) = evalTreeArray(tree.l, cX, options)
-     if !complete
-         return (cumulator, false)
-     end
-     (array2, complete2) = evalTreeArray(tree.r, cX, options)
-     if !complete2
-         return (cumulator, false)
-     end
-     op = options.binops[op_idx]
-     @inbounds @simd for j=1:size(cumulator, 1)
-         cumulator[j] = op(cumulator[j], array2[j])
-     end
-     return (cumulator, check_array(cumulator))
+    n = size(cX, 2)
+    (cumulator, complete) = evalTreeArray(tree.l, cX, options)
+    @return_on_false complete n T
+    (array2, complete2) = evalTreeArray(tree.r, cX, options)
+    @return_on_false complete2 n T
+    op = options.binops[op_idx]
+    finished_loop = true
+    @inbounds @simd for j=1:n
+        x = op(cumulator[j], array2[j])
+        @break_on_check x finished_loop T
+        cumulator[j] = x
+    end
+    @return_on_false finished_loop n T
+    return (cumulator, true)
+end
+
+function deg2_l1_r1_eval(tree::Node, cX::AbstractMatrix{T}, ::Val{op_idx}, ::Val{op_l_idx}, ::Val{op_r_idx}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real,op_idx,op_l_idx,op_r_idx}
+    n = size(cX, 2)
+    (cumulator, complete) = evalTreeArray(tree.l.l, cX, options)
+    @return_on_false complete n T
+    (array2, complete2) = evalTreeArray(tree.r.l, cX, options)
+    @return_on_false complete2 n T
+    op = options.binops[op_idx]
+    op_l = options.unaops[op_l_idx]
+    op_r = options.unaops[op_r_idx]
+    finished_loop = true
+    @inbounds @simd for j=1:n
+        x_l = op_l(cumulator[j])
+        @break_on_check x_l finished_loop T
+        x_r = op_r(array2[j])
+        @break_on_check x_r finished_loop T
+        x = op(x_l, x_r)
+        @break_on_check x finished_loop T
+        cumulator[j] = x
+    end
+    @return_on_false finished_loop n T
+    return (cumulator, true)
+end
+
+function deg2_l1_eval(tree::Node, cX::AbstractMatrix{T}, ::Val{op_idx}, ::Val{op_l_idx}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real,op_idx,op_l_idx}
+    n = size(cX, 2)
+    (cumulator, complete) = evalTreeArray(tree.l.l, cX, options)
+    @return_on_false complete n T
+    (array2, complete2) = evalTreeArray(tree.r, cX, options)
+    @return_on_false complete2 n T
+    op = options.binops[op_idx]
+    op_l = options.unaops[op_l_idx]
+    finished_loop = true
+    @inbounds @simd for j=1:n
+        x_l = op_l(cumulator[j])
+        @break_on_check x_l finished_loop T
+        x = op(x_l, array2[j])
+        @break_on_check x finished_loop T
+        cumulator[j] = x
+    end
+    @return_on_false finished_loop n T
+    return (cumulator, true)
+end
+
+function deg2_r1_eval(tree::Node, cX::AbstractMatrix{T}, ::Val{op_idx}, ::Val{op_r_idx}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real,op_idx,op_r_idx}
+    n = size(cX, 2)
+    (cumulator, complete) = evalTreeArray(tree.l, cX, options)
+    @return_on_false complete n T
+    (array2, complete2) = evalTreeArray(tree.r.l, cX, options)
+    @return_on_false complete2 n T
+    op = options.binops[op_idx]
+    op_r = options.unaops[op_r_idx]
+    finished_loop = true
+    @inbounds @simd for j=1:n
+        x_r = op_r(array2[j])
+        @break_on_check x_r finished_loop T
+        x = op(cumulator[j], x_r)
+        @break_on_check x finished_loop T
+        cumulator[j] = x
+    end
+    @return_on_false finished_loop n T
+    return (cumulator, true)
 end
 
 function deg1_eval(tree::Node, cX::AbstractMatrix{T}, ::Val{op_idx}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real,op_idx}
-     (cumulator, complete) = evalTreeArray(tree.l, cX, options)
-     if !complete
-         return (cumulator, false)
-     end
-     op = options.unaops[op_idx]
-     @inbounds @simd for j=1:size(cumulator, 1)
-         cumulator[j] = op(cumulator[j])
-     end
-     return (cumulator, check_array(cumulator))
+    n = size(cX, 2)
+    (cumulator, complete) = evalTreeArray(tree.l, cX, options)
+    @return_on_false complete n T
+    op = options.unaops[op_idx]
+    finished_loop = true
+    @inbounds @simd for j=1:n
+        x = op(cumulator[j])
+        @break_on_check x finished_loop T
+        cumulator[j] = x
+    end
+    @return_on_false finished_loop n T
+    return (cumulator, true)
 end
 
 function deg0_eval(tree::Node, cX::AbstractMatrix{T}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real}
-     if tree.constant
-         return (fill(convert(T, tree.val), size(cX, 2)), true)
-     else
-         return (copy(cX[tree.feature, :]), true)
-     end
+    n = size(cX, 2)
+    if tree.constant
+        return (fill(convert(T, tree.val), n), true)
+    else
+        return (copy(cX[tree.feature, :]), true)
+    end
 end
 
+function deg1_l1_eval(tree::Node, cX::AbstractMatrix{T}, ::Val{op_idx}, ::Val{op_l_idx}, options::Options)::Tuple{AbstractVector{T}, Bool} where {T<:Real,op_idx,op_l_idx}
+    n = size(cX, 2)
+    (cumulator, complete) = evalTreeArray(tree.l.l, cX, options)
+    @return_on_false complete n T
+    op = options.unaops[op_idx]
+    op_l = options.unaops[op_l_idx]
+    finished_loop = true
+    @inbounds @simd for j=1:n
+        x_l = op_l(cumulator[j])
+        @break_on_check x_l finished_loop T
+        x = op(x_l)
+        @break_on_check x finished_loop T
+        cumulator[j] = x
+    end
+    @return_on_false finished_loop n T
+    return (cumulator, true)
+end
