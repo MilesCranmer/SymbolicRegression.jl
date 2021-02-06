33d7d8f1f334f0393f983c63cada90e7d0ecbb0d
MilesCranmer <miles.cranmer@gmail.com>
2021-02-06 09:26:17 -0500

    Remove repeat distributed test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,3 @@ using SafeTestsets
 
 @safetestset "Unit tests" begin include("unittest.jl") end
 @safetestset "End to end test" begin include("full.jl") end
-@safetestset "End to end test" begin include("user_defined_operator.jl") end
