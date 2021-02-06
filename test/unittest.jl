dce352b8a046356d342b1bea2c6a6c6662819b4c
MilesCranmer <miles.cranmer@gmail.com>
2021-02-06 14:56:21 -0500

    Add tests for all operators

--- test/unittest.jl
+++ test/unittest.jl
@@ -1,7 +1,8 @@
 using SymbolicRegression, SymbolicUtils, Test, Random
 using SymbolicRegression: Options, stringTree, evalTreeArray, Dataset
 using SymbolicRegression: printTree, pow, EvalLoss, scoreFunc
-using SymbolicRegression: sub, greater
+using SymbolicRegression: plus, sub, mult, square, cube, div, log_abs, log2_abs, log10_abs, sqrt_abs, neg, greater, greater, relu, logical_or, logical_and
+
 
 x1 = 2.0
 # Initialize functions in Base....
@@ -70,3 +71,29 @@ for unaop in [cos, exp, log_abs, log2_abs, log10_abs, relu]
     end
 end
 
+
+# Generic operator tests
+types_to_test = [Float16, Float32, Float64, BigFloat]
+for T in types_to_test
+    val = T(0.5)
+    val2 = T(3.2)
+    @test sqrt_abs(val) == sqrt_abs(-val)
+    @test abs(log_abs(-val) - log(val)) < 1e-6
+    @test abs(log2_abs(-val) - log2(val)) < 1e-6
+    @test abs(log10_abs(-val) - log10(val)) < 1e-6
+    @test neg(-val) == val
+    @test sqrt_abs(val) == sqrt(val)
+    @test mult(val, val2) == val * val2
+    @test plus(val, val2) == val + val2
+    @test sub(val, val2) == val - val2
+    @test square(val) == val * val
+    @test cube(val) == val * val * val
+    @test div(val, val2) == val / val2
+    @test greater(val, val2) == T(0.0)
+    @test greater(val2, val) == T(1.0)
+    @test relu(-val) == T(0.0)
+    @test relu(val) == val
+    @test logical_or(val, val2) == T(1.0)
+    @test logical_or(T(0.0), val2) == T(1.0)
+    @test logical_and(T(0.0), val2) == T(0.0)
+end
