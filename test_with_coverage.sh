e4a3203b989b2449a2c107bdae47333b65661ded
MilesCranmer <miles.cranmer@gmail.com>
2021-02-06 10:29:34 -0500

    Install Coverage.jl

--- test_with_coverage.sh
+++ test_with_coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-julia --color=yes --inline=yes --depwarn=yes --code-coverage=user --project=. -e 'import pkg; pkg.test(coverage=true)' && \
+julia --project=. -e 'import Pkg; Pkg.add("Coverage")' && \
+    julia --color=yes --inline=yes --depwarn=yes --code-coverage=user --project=. -e 'import Pkg; Pkg.test(coverage=true)' && \
     ./test/pipelines.sh --code-coverage=user
     julia coverage.jl
