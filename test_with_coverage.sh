323cb05e56177a70d121751f0695536918d1da99
MilesCranmer <miles.cranmer@gmail.com>
2021-02-06 10:39:53 -0500

    Fewer tests

--- test_with_coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-julia --project=. -e 'import Pkg; Pkg.add("Coverage")' && \
-    julia --color=yes --inline=yes --depwarn=yes --code-coverage=user --project=. -e 'import Pkg; Pkg.test(coverage=true)' && \
-    ./test/pipelines.sh --code-coverage=user
-    julia coverage.jl
