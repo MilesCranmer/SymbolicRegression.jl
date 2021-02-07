50cecb7bab9583c583d5be0e9e19935a75e108c8
MilesCranmer <miles.cranmer@gmail.com>
2021-02-07 11:01:18 -0500

    Create tmp file for benchmark for consistency

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -2,11 +2,12 @@ using PkgBenchmark
 import SymbolicRegression
 
 results = []
+cp("benchmark/benchmarks.jl", "benchmark/.benchmarks.jl"; force=true)
 
 for i=1:10
     config = BenchmarkConfig(;id=(i == 1 ? nothing : "HEAD~$i"),
                               juliacmd=`julia -O3`,
                               env=Dict("JULIA_NUM_THREADS" => 4))
     push!(results,
-          benchmarkpkg(SymbolicRegression, config))
+          benchmarkpkg(SymbolicRegression, config; script="benchmark/.benchmarks.jl"))
 end
