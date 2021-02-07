18f51f6f0226d27394240a1832a7b7d6f957e86d
MilesCranmer <miles.cranmer@gmail.com>
2021-02-07 18:34:43 -0500

    Create docstrings for PopMember

--- src/PopMember.jl
+++ src/PopMember.jl
@@ -5,10 +5,35 @@ mutable struct PopMember{T<:Real}
     birth::Int
 end
 
+"""
+    PopMember(t::Node, score::T)
+
+Create a population member with a birth date at the current time.
+
+# Arguments
+
+- `t::Node`: The tree for the population member.
+- `score::T`: The loss to assign this member.
+"""
 function PopMember(t::Node, score::T) where {T<:Real}
     PopMember{T}(t, score, getTime())
 end
 
+"""
+    PopMember(dataset::Dataset{T},
+              baseline::T, t::Node,
+              options::Options)
+
+Create a population member with a birth date at the current time.
+Automatically compute the score for this tree.
+
+# Arguments
+
+- `dataset::Dataset{T}`: The dataset to evaluate the tree on.
+- `baseline::T`: The baseline loss.
+- `t::Node`: The tree for the population member.
+- `options::Options`: What options to use.
+"""
 function PopMember(dataset::Dataset{T},
                    baseline::T, t::Node,
                    options::Options) where {T<:Real}
