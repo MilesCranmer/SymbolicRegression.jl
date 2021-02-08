3094cc8d5bd17f7cf64456c00033ce360ec2ec3c
MilesCranmer <miles.cranmer@gmail.com>
2021-02-07 19:31:45 -0500

    Add file to produce docs

---
+++ docs/make.jl
@@ -0,0 +1,4 @@
+using Documenter, SymbolicRegression
+using SymbolicRegression: Node, PopMember, Population, evalTreeArray, Dataset, HallOfFame, CONST_TYPE, stringTree
+
+makedocs(sitename="SymbolicRegression.jl")
