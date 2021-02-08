9431634e4302be9b1792ac392a61b60fb59f9156
MilesCranmer <miles.cranmer@gmail.com>
2021-02-07 19:44:27 -0500

    Deploy site as part of docs building

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,8 @@
 using Documenter, SymbolicRegression
 using SymbolicRegression: Node, PopMember, Population, evalTreeArray, Dataset, HallOfFame, CONST_TYPE, stringTree
 
-makedocs(sitename="SymbolicRegression.jl")
+makedocs(sitename="SymbolicRegression.jl",
+         authors="Miles Cranmer",
+         doctest=false, clean=true)
+
+deploydocs(repo="github.com/MilesCranmer/SymbolicRegression.jl.git")
