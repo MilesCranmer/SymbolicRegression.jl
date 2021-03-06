90f7956e45d149aa5b6ab30253be943047cd3d69
MilesCranmer <miles.cranmer@gmail.com>
2021-03-06 04:11:05 -0500

    Export special function operators

--- src/SymbolicRegression.jl
+++ src/SymbolicRegression.jl
@@ -40,7 +40,14 @@ export Population,
     greater,
     relu,
     logical_or,
-    logical_and
+    logical_and,
+
+    # special operators
+    gamma,
+    lgamma,
+    erf,
+    erfc,
+    beta
 
 using Distributed
 using Printf: @printf
@@ -50,7 +57,7 @@ using FromFile
 using Reexport
 @reexport using LossFunctions
 
-@from "Core.jl" import CONST_TYPE, maxdegree, Dataset, Node, copyNode, Options, plus, sub, mult, square, cube, pow, div, log_abs, log2_abs, log10_abs, sqrt_abs, neg, greater, greater, relu, logical_or, logical_and
+@from "Core.jl" import CONST_TYPE, maxdegree, Dataset, Node, copyNode, Options, plus, sub, mult, square, cube, pow, div, log_abs, log2_abs, log10_abs, sqrt_abs, neg, greater, greater, relu, logical_or, logical_and, gamma, lgamma, erf, erfc, beta
 @from "Utils.jl" import debug, debug_inline, is_anonymous_function
 @from "EquationUtils.jl" import countNodes, printTree, stringTree
 @from "EvaluateEquation.jl" import evalTreeArray, differentiableEvalTreeArray
