4b790e2b470cce6ab42ad09be493b333bff15a6c
MilesCranmer <miles.cranmer@gmail.com>
2021-06-19 14:39:38 -0400

    Bug where recorder is set to false

--- src/Options.jl
+++ src/Options.jl
@@ -389,8 +389,6 @@ function Options(;
 
     if recorder == nothing
         recorder = haskey(ENV, "PYSR_RECORDER") && (ENV["PYSR_RECORDER"] == "1")
-    else
-        recorder = false
     end
 
     options = Options{typeof(binary_operators),typeof(unary_operators), typeof(loss)}(binary_operators, unary_operators, bin_constraints, una_constraints, ns, parsimony, alpha, maxsize, maxdepth, fast_cycle, migration, hofMigration, fractionReplacedHof, shouldOptimizeConstants, hofFile, npopulations, perturbationFactor, annealing, batching, batchSize, mutationWeights, warmupMaxsizeBy, useFrequency, npop, ncyclesperiteration, fractionReplaced, topn, verbosity, probNegate, nuna, nbin, seed, loss, progress, terminal_width, optimizer_algorithm, optimize_probability, optimizer_nrestarts, optimizer_iterations, recorder, recorder_file, probPickFirst)
