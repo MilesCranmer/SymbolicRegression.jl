68dcf8f37b0253950e6fefddbeb1659140b9b718
MilesCranmer <miles.cranmer@gmail.com>
2021-07-08 17:23:59 -0400

    Clean up documentation

--- src/InterfaceSymbolicUtils.jl
+++ src/InterfaceSymbolicUtils.jl
@@ -5,30 +5,6 @@ using SymbolicUtils
 
 const SYMBOLIC_UTILS_TYPES = Union{<:Number,SymbolicUtils.Sym{<:Number},SymbolicUtils.Term{<:Number}}
 
-"""
-    node_to_symbolic(tree::Node, options::Options;
-                varMap::Union{Array{String, 1}, Nothing}=nothing,
-                evaluate_functions::Bool=false,
-                index_functions::Bool=false)
-
-The interface to SymbolicUtils.jl. Passing a tree to this function
-will generate a symbolic equation in SymbolicUtils.jl format.
-
-## Arguments
-
-- `tree::Node`: The equation to convert.
-- `options::Options`: Options, which contains the operators used in the equation.
-- `varMap::Union{Array{String, 1}, Nothing}=nothing`: What variable names to use for
-    each feature. Default is [x1, x2, x3, ...].
-- `evaluate_functions::Bool=false`: Whether to evaluate the operators, or
-    leave them as symbolic.
-- `index_functions::Bool=false`: Whether to generate special names for the
-    operators, which then allows one to convert back to a `Node` format
-    using `symbolic_to_node`.
-"""
-
-
-
 const SUPPORTED_OPS = (cos, sin, exp, cot, tan, csc, sec, +, -, *, /)
 
 isgood(x::SymbolicUtils.Symbolic) = SymbolicUtils.istree(x) ? all(isgood.([SymbolicUtils.operation(x);SymbolicUtils.arguments(x)])) : true
@@ -109,6 +85,27 @@ function Base.convert(::typeof(Node), x::SymbolicUtils.Symbolic, opts)
     return Node(ind, map(x->convert(Node, x, opts), args)...)
 end
 
+"""
+    node_to_symbolic(tree::Node, options::Options;
+                varMap::Union{Array{String, 1}, Nothing}=nothing,
+                evaluate_functions::Bool=false,
+                index_functions::Bool=false)
+
+The interface to SymbolicUtils.jl. Passing a tree to this function
+will generate a symbolic equation in SymbolicUtils.jl format.
+
+## Arguments
+
+- `tree::Node`: The equation to convert.
+- `options::Options`: Options, which contains the operators used in the equation.
+- `varMap::Union{Array{String, 1}, Nothing}=nothing`: What variable names to use for
+    each feature. Default is [x1, x2, x3, ...].
+- `evaluate_functions::Bool=false`: Whether to evaluate the operators, or
+    leave them as symbolic.
+- `index_functions::Bool=false`: Whether to generate special names for the
+    operators, which then allows one to convert back to a `Node` format
+    using `symbolic_to_node`.
+"""
 function node_to_symbolic(tree::Node, options::Options;
                      varMap::Union{Array{String, 1}, Nothing}=nothing,
                      evaluate_functions::Bool=false,
@@ -136,94 +133,3 @@ function symbolic_to_node(eqn::T, options::Options;
     )
     convert(Node, substitute(eqn, subs), options)
 end
-
-# Just variable
-#function symbolic_to_node(eqn::T, options::Options;
-#                     varMap::Union{Array{String, 1}, Nothing}=nothing)::Node where {T<:SymbolicUtils.Sym{<:Number}}
-#    return Node(varMap_to_index(eqn.name, varMap))
-#end
-#
-#function _multiarg_split(op_idx::Int, eqn::Array{Any, 1},
-#                        options::Options, varMap::Union{Array{String, 1}, Nothing}
-#                       )::Node
-#    if length(eqn) == 2
-#        return Node(op_idx,
-#                    symbolic_to_node(eqn[1], options, varMap=varMap),
-#                    symbolic_to_node(eqn[2], options, varMap=varMap))
-#    elseif length(eqn) == 3
-#        return Node(op_idx,
-#                    symbolic_to_node(eqn[1], options, varMap=varMap),
-#                    _multiarg_split(op_idx, eqn[2:3], options, varMap))
-#    else
-#        # Minimize depth:
-#        split_point = round(Int, length(eqn) // 2)
-#        return Node(op_idx,
-#                    _multiarg_split(op_idx, eqn[1:split_point], options, varMap),
-#                    _multiarg_split(op_idx, eqn[split_point+1:end], options, varMap))
-#    end
-#end
-#
-## Equation:
-#function symbolic_to_node(eqn::T, options::Options;
-#                       varMap::Union{Array{String, 1}, Nothing}=nothing
-#                  )::Node where {T<:SymbolicUtils.Term{<:Number}}
-#    args = SymbolicUtils.arguments(eqn)
-#    l = symbolic_to_node(args[1], options, varMap=varMap)
-#    nargs = length(args)
-#    op = SymbolicUtils.operation(eqn)
-#    if nargs == 1
-#        op_idx = unaop_to_index(op, options)
-#        return Node(op_idx, l)
-#    else
-#        op_idx = binop_to_index(op, options)
-#        if nargs == 2
-#            r = symbolic_to_node(args[2], options, varMap=varMap)
-#            return Node(op_idx, l, r)
-#        else
-#            # TODO: Assert operator is +, *
-#            return _multiarg_split(op_idx, args, options, varMap)
-#        end
-#    end
-#end
-#
-#function unaop_to_index(op::F, options::Options)::Int where {F<:SymbolicUtils.Sym}
-#    # In format _unaop1
-#    parse(Int, string(op.name)[7:end])
-#end
-#
-#function binop_to_index(op::F, options::Options)::Int where {F<:SymbolicUtils.Sym}
-#    # In format _binop1
-#    parse(Int, string(op.name)[7:end])
-#end
-#
-#function unaop_to_index(op::F, options::Options)::Int where {F<:Function}
-#    for i=1:options.nuna
-#        if op == options.unaops[i]
-#            return i
-#        end
-#    end
-#    error("Operator $(op) in simplified expression not found in options $(options.unaops)!")
-#end
-#
-#function binop_to_index(op::F, options::Options)::Int where {F<:Function}
-#    for i=1:options.nbin
-#        if op == options.binops[i]
-#            return i
-#        end
-#    end
-#    error("Operator $(op) in simplified expression not found in options $(options.binops)!")
-#end
-#
-#function varMap_to_index(var::Symbol, varMap::Array{String, 1})::Int
-#    str = string(var)
-#    for i=1:length(varMap)
-#        if str == varMap[i]
-#            return i
-#        end
-#    end
-#end
-#
-#function varMap_to_index(var::Symbol, varMap::Nothing)::Int
-#    return parse(Int, string(var)[2:end])
-#end
-#
