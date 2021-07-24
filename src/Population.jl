d1f08e6aab4a7dc59d133a15010047c13f6beab1
Kaze Wong <kazewong@jhu.edu>
2021-07-24 14:11:14 -0400

    Add iniitalization method to population without a scoring function.

--- src/Population.jl
+++ src/Population.jl
@@ -16,6 +16,21 @@ end
 Create population from list of PopMembers.
 """
 Population(pop::Array{PopMember{T}, 1}) where {T<:Real} = Population{T}(pop, size(pop, 1))
+
+"""
+    Population(dataset::Dataset{T}, baseline::T;
+               npop::Int, nlength::Int=3, options::Options,
+               nfeatures::Int)
+
+Create random population and with baseline score.
+"""
+
+Population(baseline::T;
+           npop::Int, nlength::Int=3,
+           options::Options,
+           nfeatures::Int) where {T<:Real} = Population([PopMember(genRandomTree(nlength, options, nfeatures), baseline) for i=1:npop], npop)
+
+
 """
     Population(dataset::Dataset{T}, baseline::T;
                npop::Int, nlength::Int=3, options::Options,
