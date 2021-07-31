96a371beb950354dc068978dda0f35192f7cfbf7
MilesCranmer <miles.cranmer@gmail.com>
2021-07-31 02:02:18 +0000

    Test custom operator gradient

--- test/derivative_test.jl
+++ test/derivative_test.jl
@@ -4,21 +4,42 @@ using SymbolicRegression: evalDiffTreeArray, evalGradTreeArray
 using Zygote
 using LinearAlgebra
 
+input_data = [1.0 2.0 3.0]
+input_matrix = transpose(input_data)
+
+custom_cos(x) = cos(x)^2
+
+options = Options(;
+    binary_operators=(+, *, -, /, ^),
+    unary_operators=(custom_cos, exp),
+)
+equation(x1, x2, x3) =  (abs(abs(3.0 * custom_cos(x1)) ^ 2.3) ^ (-1.2)) + x2 - x3
+true_grad = gradient(equation, input_data[1, :]...)
+
+tree = Node(5, (Node(3.0) * Node(1, Node("x1"))) ^ 2.3, -1.2) + Node("x2") - Node("x3")
+
+for i=1:3
+    @test isapprox(evalDiffTreeArray(tree, input_matrix, options, i)[2][1], true_grad[i], rtol=1e-5)
+end
+
+for i=1:3
+    @test isapprox(evalGradTreeArray(tree, input_matrix, options; variable=true)[2][i, 1], true_grad[i], rtol=1e-5)
+end
+
+
 options = Options(;
     binary_operators=(+, *, -, /, ^),
     unary_operators=(cos, exp),
 )
+equation(x1, x2, x3) =  (abs(abs(3.0 * cos(x1)) ^ 2.3) ^ (-1.2)) + x2 - x3
+true_grad = gradient(equation, input_data[1, :]...)
+
 tree = Node(5, (Node(3.0) * Node(1, Node("x1"))) ^ 2.3, -1.2) + Node("x2") - Node("x3")
 
-equation(x1, x2, x3) =  (abs(abs(3.0 * cos(x1)) ^ 2.3) ^ (-1.2)) + x2 - x3
-
-input_data = transpose([1.0 2.0 3.0])
-true_grad = gradient(equation, 1.0, 2.0, 3.0)
-
 for i=1:3
-    @test evalDiffTreeArray(tree, input_data, options, i)[2][1] ≈ true_grad[i]
+    @test isapprox(evalDiffTreeArray(tree, input_matrix, options, i)[2][1], true_grad[i], rtol=1e-5)
 end
 
 for i=1:3
-    @test evalGradTreeArray(tree, input_data, options; variable=true)[2][i, 1] ≈ true_grad[i]
+    @test isapprox(evalGradTreeArray(tree, input_matrix, options; variable=true)[2][i, 1], true_grad[i], rtol=1e-5)
 end
