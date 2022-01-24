a0df93a105476e8796d1f602c09986418204351e
MilesCranmer <miles.cranmer@gmail.com>
2022-01-23 22:00:50 -0500

    Beef up simplification test

--- test/test_simplification.jl
+++ test/test_simplification.jl
@@ -1,6 +1,10 @@
 using SymbolicRegression, Test
 
-options = Options(binary_operators=(+, -, /, *))
+binary_operators = (+, -, /, *)
+
+index_of_mult = [i for (i, op) in enumerate(binary_operators) if op == *][1]
+
+options = Options(binary_operators=binary_operators)
 
 tree = Node("x1") + Node("x1")
 
@@ -9,3 +13,9 @@ eqn = node_to_symbolic(tree, options; index_functions=true)
 eqn2 = custom_simplify(eqn, options)
 
 @test "$(eqn2[1])" == "2x1"
+
+# Repeat test with simplifyWithSymbolicUtils:
+simple_tree = simplifyWithSymbolicUtils(tree, options, 5)
+
+# Check that the first operator is *, for 2 * x1:
+@test simple_tree.op == index_of_mult
