3ef4105f7f19fd73ade991aeadd9d53a31a21fd1
MilesCranmer <miles.cranmer@gmail.com>
2022-04-09 17:50:15 -0400

    Clean up derivative tests

--- test/derivative_test.jl
+++ test/derivative_test.jl
@@ -18,28 +18,18 @@ true_grad = gradient(equation, input_data[1, :]...)
 
 tree = Node(5, (Node(3.0) * Node(1, Node("x1"))) ^ 2.3, -1.2) + Node("x2") - Node("x3")
 
+# Simple tests of derivatives with respect to variables
 for i=1:3
-    @test isapprox(evalDiffTreeArray(tree, input_matrix, options, i)[2][1], true_grad[i], rtol=1e-5)
-end
 
-for i=1:3
-    @test isapprox(evalGradTreeArray(tree, input_matrix, options; variable=true)[2][i, 1], true_grad[i], rtol=1e-5)
-end
+    @test isapprox(
+        evalDiffTreeArray(tree, input_matrix, options, i)[2][1],
+        true_grad[i],
+        rtol=1e-5
+    )
 
-
-options = Options(;
-    binary_operators=(+, *, -, /, ^),
-    unary_operators=(cos, exp),
-)
-equation(x1, x2, x3) =  (abs(abs(3.0 * cos(x1)) ^ 2.3) ^ (-1.2)) + x2 - x3
-true_grad = gradient(equation, input_data[1, :]...)
-
-tree = Node(5, (Node(3.0) * Node(1, Node("x1"))) ^ 2.3, -1.2) + Node("x2") - Node("x3")
-
-for i=1:3
-    @test isapprox(evalDiffTreeArray(tree, input_matrix, options, i)[2][1], true_grad[i], rtol=1e-5)
-end
-
-for i=1:3
-    @test isapprox(evalGradTreeArray(tree, input_matrix, options; variable=true)[2][i, 1], true_grad[i], rtol=1e-5)
+    @test isapprox(
+        evalGradTreeArray(tree, input_matrix, options; variable=true)[2][i, 1],
+        true_grad[i],
+        rtol=1e-5
+    )
 end
