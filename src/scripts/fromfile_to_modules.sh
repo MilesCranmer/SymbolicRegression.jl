aa64907e676850e5aa3610b49a3836d5dcf2c2c1
MilesCranmer <miles.cranmer@gmail.com>
2022-04-23 17:18:03 -0400

    Fix import level in FromFile converter

--- src/scripts/fromfile_to_modules.sh
+++ src/scripts/fromfile_to_modules.sh
@@ -7,6 +7,6 @@ FILES=$@
 # Loop through files:
 for file in $FILES; do
     base=$(basename ${file%.*})
-    cat $file | vims -t '%g/^@from/s/@from "\(.\{-}\)\.jl" import/import .\1:/g' -e 'using FromFile' 'dd' -s "Omodule ${base}\<enter>" 'Go\<enter>end' > tmp.jl
+    cat $file | vims -t '%g/^@from/s/@from "\(.\{-}\)\.jl" import/import ..\1:/g' -e 'using FromFile' 'dd' -s "Omodule ${base}\<enter>" 'Go\<enter>end' > tmp.jl
     mv tmp.jl $file
 done
