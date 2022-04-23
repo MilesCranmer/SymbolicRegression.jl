f8160a4db84ccc95909002e1c22ad35cdede0100
MilesCranmer <miles.cranmer@gmail.com>
2022-04-23 17:19:58 -0400

    Fix fromfile conversion script

--- src/scripts/fromfile_to_modules.sh
+++ src/scripts/fromfile_to_modules.sh
@@ -7,7 +7,7 @@ FILES=$@
 # Loop through files:
 for file in $FILES; do
     base=$(basename ${file%.*})
-    cat $file | vims -t '%g/^@from/s/@from "\(.\{-}\)\.jl" import/import ..\1:/g' -e 'using FromFile' 'dd' -s "Omodule ${base}\<enter>" 'Go\<enter>end' > tmp.jl
+    cat $file | vims -t '%g/^@from/s/@from "\(.\{-}\)\.jl" import/import .._\1:/g' -e 'using FromFile' 'dd' -s "Omodule _${base}\<enter>" 'Go\<enter>end' > tmp.jl
     mv tmp.jl $file
 done
 
