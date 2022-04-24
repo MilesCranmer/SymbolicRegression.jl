bda3e7e2a69ab40703c30f9984fe3d9a1a5a7f13
MilesCranmer <miles.cranmer@gmail.com>
2022-04-23 23:59:30 -0400

    Speed up nested calculation

--- src/CheckConstraints.jl
+++ src/CheckConstraints.jl
@@ -61,54 +61,55 @@ function count_operators(tree::Node, ::Val{degree}, ::Val{op}, options::Options)
     return count
 end
 
+
 """Count the max number of times an operator of a given degree is nested"""
-function count_max_nestedness(tree::Node, ::Val{degree}, ::Val{op}, options::Options)::Int where {op,degree}
+function count_max_nestedness(tree::Node, degree::Int, op::Int, options::Options)::Int
     if tree.degree == 0
         return 0
     elseif tree.degree == 1
         count = (degree == 1 && tree.op == op) ? 1 : 0
-        return count + count_max_nestedness(tree.l, Val(degree), Val(op), options)
+        return count + count_max_nestedness(tree.l, degree, op, options)
     else  # tree.degree == 2
         count = (degree == 2 && tree.op == op) ? 1 : 0
         return count + max(
-            count_max_nestedness(tree.l, Val(degree), Val(op), options),
-            count_max_nestedness(tree.r, Val(degree), Val(op), options)
+            count_max_nestedness(tree.l, degree, op, options),
+            count_max_nestedness(tree.r, degree, op, options)
         )
     end
 end
 
 # function fast_max_nestedness(tree::Node, degree::Int, op_idx::Int, nested_degree::Int, nested_op_idx::Int, options::Options)::Int
-function fast_max_nestedness(tree::Node, ::Val{degree}, ::Val{op_idx}, ::Val{nested_degree}, ::Val{nested_op_idx}, options::Options)::Int where {degree,op_idx,nested_degree,nested_op_idx}
+function fast_max_nestedness(tree::Node, degree::Int, op_idx::Int, nested_degree::Int, nested_op_idx::Int, options::Options)::Int
     # Don't need to branch - once you find operator, run
     # count_max once, then return. Don't need to go deeper!
     if tree.degree == 0
         return 0
     elseif tree.degree == 1
         if degree != tree.degree || tree.op != op_idx
-            return fast_max_nestedness(tree.l, Val(degree), Val(op_idx), Val(nested_degree), Val(nested_op_idx), options)
+            return fast_max_nestedness(tree.l, degree, op_idx, nested_degree, nested_op_idx, options)
         end
-        return count_max_nestedness(tree.l, Val(nested_degree), Val(nested_op_idx), options)
+        return count_max_nestedness(tree.l, nested_degree, nested_op_idx, options)
     else
         if degree != tree.degree || tree.op != op_idx
             return max(
-                fast_max_nestedness(tree.l, Val(degree), Val(op_idx), Val(nested_degree), Val(nested_op_idx), options),
-                fast_max_nestedness(tree.r, Val(degree), Val(op_idx), Val(nested_degree), Val(nested_op_idx), options)
+                fast_max_nestedness(tree.l, degree, op_idx, nested_degree, nested_op_idx, options),
+                fast_max_nestedness(tree.r, degree, op_idx, nested_degree, nested_op_idx, options)
             )
         end
         return max(
-            count_max_nestedness(tree.l, Val(nested_degree), Val(nested_op_idx), options),
-            count_max_nestedness(tree.r, Val(nested_degree), Val(nested_op_idx), options)
+            count_max_nestedness(tree.l, nested_degree, nested_op_idx, options),
+            count_max_nestedness(tree.r, nested_degree, nested_op_idx, options)
         )
     end
 end
 
 """Check if there are any illegal combinations of operators"""
-function flag_illegal_nests(tree::Node, options::Options{A,B,dA,dB,S})::Bool where {A,B,dA,dB,S}
+function flag_illegal_nests(tree::Node, options::Options)::Bool
     # We search from the top first, then from child nodes at end.
     nested_constraints = options.nested_constraints
-    for (degree, op_idx, nested_constraint) ∈ nested_constraints
-        for (nested_degree, nested_op_idx, max_nestedness) ∈ nested_constraint
-            nestedness = fast_max_nestedness(tree, Val(degree), Val(op_idx), Val(nested_degree), Val(nested_op_idx), options)
+    for (degree, op_idx, op_constraint) ∈ nested_constraints
+        for (nested_degree, nested_op_idx, max_nestedness) ∈ op_constraint
+            nestedness = fast_max_nestedness(tree, degree, op_idx, nested_degree, nested_op_idx, options)
             if nestedness > max_nestedness
                 return true
             end
