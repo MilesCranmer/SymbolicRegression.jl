4af3af36a37a18f70a201e9cded6f911bf10238a
MilesCranmer <miles.cranmer@gmail.com>
2022-05-02 08:29:19 -0400

    Build deprecation library

--- src/Deprecates.jl
+++ src/Deprecates.jl
@@ -1,9 +1,60 @@
-# Will be included explicitly
 using Base: @deprecate
 
-# Now the batch dimension is the last axis!
-@deprecate RunSR(X, y; kw...) EquationSearch(copy(transpose(X)), y; kw...)
-@deprecate sqrtm(x) sqrt_abs(x)
-@deprecate logm(x) log_abs(x)
-@deprecate logm2(x) log2_abs(x)
-@deprecate logm10(x) log10_abs(x)
+
+@deprecate EvalLoss eval_loss
+@deprecate Loss loss
+@deprecate OptimizeAndSimplifyPopulation optimize_and_simplify_population
+@deprecate SRCycle s_r_cycle
+@deprecate _evalDiffTreeArray _eval_diff_tree_array
+@deprecate _evalGradTreeArray _eval_grad_tree_array
+@deprecate _evalTreeArray _eval_tree_array
+@deprecate appendRandomOp append_random_op
+@deprecate bestOfSample best_of_sample
+@deprecate bestSubPop best_sub_pop
+@deprecate calculateParetoFrontier calculate_pareto_frontier
+@deprecate combineOperators combine_operators
+@deprecate copyNode copy_node
+@deprecate copyPopMember copy_pop_member
+@deprecate countBinaryOperators count_binary_operators
+@deprecate countConstants count_constants
+@deprecate countDepth count_depth
+@deprecate countNodes count_nodes
+@deprecate countOperators count_operators
+@deprecate countUnaryOperators count_unary_operators
+@deprecate crossoverGeneration crossover_generation
+@deprecate crossoverTrees crossover_trees
+@deprecate deleteRandomOp delete_random_op
+@deprecate differentiableEvalTreeArray differentiable_eval_tree_array
+@deprecate evalDiffTreeArray eval_diff_tree_array
+@deprecate evalGradTreeArray eval_grad_tree_array
+@deprecate evalTreeArray eval_tree_array
+@deprecate finalizeScores finalize_scores
+@deprecate flagBinOperatorComplexity flag_bin_operator_complexity
+@deprecate flagUnaOperatorComplexity flag_una_operator_complexity
+@deprecate genRandomTree gen_random_tree
+@deprecate genRandomTreeFixedSize gen_random_tree_fixed_size
+@deprecate getConstants get_constants
+@deprecate getTime get_time
+@deprecate indexConstants index_constants
+@deprecate insertRandomOp insert_random_op
+@deprecate lossToScore loss_to_score
+@deprecate makeRandomLeaf make_random_leaf
+@deprecate mutateConstant mutate_constant
+@deprecate mutateOperator mutate_operator
+@deprecate nextGeneration next_generation
+@deprecate optFunc opt_func
+@deprecate optimizeConstants optimize_constants
+@deprecate prependRandomOp prepend_random_op
+@deprecate printTree print_tree
+@deprecate randomNode random_node
+@deprecate randomNodeAndParent random_node_and_parent
+@deprecate regEvolCycle reg_evol_cycle
+@deprecate samplePop sample_pop
+@deprecate scoreFunc score_func
+@deprecate scoreFuncBatch score_func_batch
+@deprecate setConstants set_constants
+@deprecate simplifyTree simplify_tree
+@deprecate stringOp string_op
+@deprecate stringTree string_tree
+@deprecate testDatasetConfiguration test_dataset_configuration
+@deprecate testOptionConfiguration test_option_configuration
