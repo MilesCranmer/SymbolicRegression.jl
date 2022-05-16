7ded5a1ffe5c2101f65f36825047065cbbb66cf1
MilesCranmer <miles.cranmer@gmail.com>
2022-05-16 01:47:09 -0400

    Test custom operator in all tests

--- test/test_params.jl
+++ test/test_params.jl
@@ -1,10 +1,11 @@
 import SymbolicRegression: L2DistLoss
 
 maximum_residual = 1e-2
+custom_cos(x) = cos(x)
 
 default_params = (
     binary_operators=(/, +, *),
-    unary_operators=(exp, cos),
+    unary_operators=(exp, custom_cos),
     constraints=nothing,
     loss=L2DistLoss(),
     ns=10,
