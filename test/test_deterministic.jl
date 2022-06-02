faf0a0e8373aee2925e84226b201881ca68604e3
MilesCranmer <miles.cranmer@gmail.com>
2022-06-02 01:33:02 -0400

    Put deterministic test within local scope

--- test/test_deterministic.jl
+++ test/test_deterministic.jl
@@ -2,27 +2,29 @@ using SymbolicRegression
 using Test
 using Random
 
-X = 2 .* randn(MersenneTwister(0), Float32, 2, 1000)
-y = 3 * cos.(X[2, :]) + X[1, :] .^ 2 .- 2
+begin
+    X = 2 .* randn(MersenneTwister(0), Float32, 2, 1000)
+    y = 3 * cos.(X[2, :]) + X[1, :] .^ 2 .- 2
 
-options = SymbolicRegression.Options(;
-    binary_operators=(+, *, /, -),
-    unary_operators=(cos,),
-    crossoverProbability=0.0,  # required for recording, as not set up to track crossovers.
-    max_evals=10000,
-    deterministic=true,
-    seed=0,
-    verbosity=0,
-    progress=false,
-)
-
-all_outputs = []
-for i in 1:2
-    hall_of_fame = EquationSearch(
-        X, y; niterations=5, options=options, multithreading=false, numprocs=0
+    options = SymbolicRegression.Options(;
+        binary_operators=(+, *, /, -),
+        unary_operators=(cos,),
+        crossoverProbability=0.0,  # required for recording, as not set up to track crossovers.
+        max_evals=10000,
+        deterministic=true,
+        seed=0,
+        verbosity=0,
+        progress=false,
     )
-    dominating = calculate_pareto_frontier(X, y, hall_of_fame, options)
-    push!(all_outputs, dominating[end].tree)
-end
 
-@test string(all_outputs[1]) == string(all_outputs[2])
+    all_outputs = []
+    for i in 1:2
+        hall_of_fame = EquationSearch(
+            X, y; niterations=5, options=options, multithreading=false, numprocs=0
+        )
+        dominating = calculate_pareto_frontier(X, y, hall_of_fame, options)
+        push!(all_outputs, dominating[end].tree)
+    end
+
+    @test string(all_outputs[1]) == string(all_outputs[2])
+end
