0024f4e15508a688cd3118efd33a015db9da8fd5
Jgmedina95 <97254349+Jgmedina95@users.noreply.github.com>
2022-08-01 08:55:01 -0400

    correct ...functions to function in Option constructor

--- src/Options.jl
+++ src/Options.jl
@@ -266,7 +266,7 @@ function Options(;
     unary_operators::NTuple{nuna,Any}=(),
     constraints=nothing,
     loss=L2DistLoss(),
-    custom_loss_functions=nothing,
+    custom_loss_function=nothing,
     ns=12, #1 sampled from every ns per mutation
     topn=12, #samples to return per population
     complexity_of_operators=nothing,
