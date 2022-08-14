e180f72a5c1604e3596c9135a54e6fab8c87ef1f
MilesCranmer <miles.cranmer@gmail.com>
2022-08-13 23:59:51 -0400

    Remove atomic operations from loss cache

--- src/LossCache.jl
+++ src/LossCache.jl
@@ -9,13 +9,12 @@ Has a max size and will delete members with FIFO order.
 """
 mutable struct LossCache{T<:Real}
     cache::Dict{UInt,T}
-    thread_lock::Threads.SpinLock
     fifo_keys::Vector{UInt}
     max_size::Int
 end
 
 function LossCache(cache::Dict{UInt,T}; max_size::Int=100_000) where {T<:Real}
-    return LossCache{T}(cache, Threads.SpinLock(), UInt[], max_size)
+    return LossCache{T}(cache, UInt[], max_size)
 end
 
 """Removes the oldest entry from the cache."""
@@ -28,37 +27,29 @@ function _unsafe_trim!(cache::LossCache{T}) where {T<:Real}
 end
 
 function Base.haskey(cache::LossCache{T}, key::UInt)::Bool where {T<:Real}
-    lock(cache.thread_lock) do
-        return haskey(cache.cache, key)
-    end
+    return haskey(cache.cache, key)
 end
 
 function Base.getindex(cache::LossCache{T}, key::UInt)::T where {T<:Real}
-    lock(cache.thread_lock) do
-        return cache.cache[key]
-    end
+    return cache.cache[key]
 end
 
 function Base.setindex!(cache::LossCache{T}, value::T, key::UInt) where {T<:Real}
-    lock(cache.thread_lock) do
-        cache.cache[key] = value
-        push!(cache.fifo_keys, key)
-        _unsafe_trim!(cache)
-    end
+    cache.cache[key] = value
+    push!(cache.fifo_keys, key)
+    _unsafe_trim!(cache)
 end
 
 function Base.get!(f::Function, cache::LossCache{T}, key::UInt) where {T<:Real}
-    lock(cache.thread_lock) do
-        if haskey(cache.cache, key)
-            output = cache.cache[key]
-        else
-            output = f()
-            cache.cache[key] = output
-            push!(cache.fifo_keys, key)
-            _unsafe_trim!(cache)
-        end
-        return output
+    if haskey(cache.cache, key)
+        output = cache.cache[key]
+    else
+        output = f()
+        cache.cache[key] = output
+        push!(cache.fifo_keys, key)
+        _unsafe_trim!(cache)
     end
+    return output
 end
 
 function maybe_get!(
