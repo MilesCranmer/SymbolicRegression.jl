fdc9d966ab3a4d93b7c6f1a4f2907acf1ee8d3fc
MilesCranmer <miles.cranmer@gmail.com>
2022-08-21 22:46:22 -0400

    Attempt to create MWE

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,38 @@
 using SafeTestsets
 
-@safetestset "Unit tests" begin
-    include("unittest.jl")
+# HACK
+using Pkg
+using Distributed
+import ReverseDiff
+procs = addprocs(4)
+
+# Activate env on workers:
+project_path = splitdir(Pkg.project().path)[1]
+@everywhere procs begin
+    Base.MainInclude.eval(
+        quote
+            using Pkg
+            Pkg.activate($$project_path)
+        end,
+    )
 end
-@safetestset "End to end test" begin
-    include("full.jl")
+
+# Import package on workers:
+@everywhere procs begin
+    Base.MainInclude.eval(import ReverseDiff)
+end
+
+# Import SymbolicRegression on workers:
+@everywhere procs begin
+    Base.MainInclude.eval(import ReverseDiff)
+end
+# END HACK
+
+if false
+	@safetestset "Unit tests" begin
+		include("unittest.jl")
+	end
+	@safetestset "End to end test" begin
+		include("full.jl")
+	end
 end
