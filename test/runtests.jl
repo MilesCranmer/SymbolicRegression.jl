89a1a63caea28472d741e0432199e1a83758918f
MilesCranmer <miles.cranmer@gmail.com>
2022-08-22 12:51:58 -0400

    Test variety of packages in worker process

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,42 @@
 # HACK
 using Pkg, Distributed
-procs = addprocs(4)
 
-# Activate env on workers:
-project_path = splitdir(Pkg.project().path)[1]
-@everywhere procs begin
-    Base.MainInclude.eval(
-        quote
-            using Pkg
-            Pkg.activate($$project_path)
-            # Import package on workers:
-            import ReverseDiff
-        end,
-    )
+"""Try to dynamically create workers, and import the package."""
+function test(package_name)
+    procs = addprocs(2)
+    project_path = splitdir(Pkg.project().path)[1]
+    @everywhere procs begin
+        Base.MainInclude.eval(
+            quote
+                using Pkg
+                Pkg.activate($$project_path)
+                # Import package on workers:
+                using $(Symbol($package_name)): $(Symbol($package_name))
+            end,
+        )
+    end
+    rmprocs(procs)
 end
-# END HACK
+
+packages_to_test = [
+    "Distributed",
+    "JSON3",
+    "LineSearches",
+    "LinearAlgebra",
+    "LossFunctions",
+    "Optim",
+    "Printf",
+    "Random",
+    "Reexport",
+    "SpecialFunctions",
+    "Zygote",
+    "ReverseDiff",
+    "SymbolicUtils",
+    "PreallocationTools",
+    "SymbolicRegression",
+]
+for package_name in packages_to_test
+    println("Testing $(package_name)...")
+    test(package_name)
+    println("Success!")
+end
