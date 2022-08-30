32c3354b570d8f16adf8ebdb7d8a45717428ea36
MilesCranmer <miles.cranmer@gmail.com>
2022-08-30 10:30:33 -0400

    Expand NaN detection tests to multiple types

--- test/test_nan_detection.jl
+++ test/test_nan_detection.jl
@@ -1,22 +1,29 @@
 println("Testing NaN detection.")
 
-# Creating a NaN via computation.
-tree = cos(exp(exp(exp(exp(Node("x1"))))))
-X = randn(MersenneTwister(0), Float32, 1, 100) * 100.0f0
-output, flag = eval_tree_array(tree, X, options)
-@test !flag
+for T in [Float16, Float32, Float64]
+    options = Options(binary_operators=(+, *, /, -), unary_operators=(cos, sin, exp))
+    # Creating a NaN via computation.
+    tree = cos(exp(exp(exp(exp(Node("x1"))))))
+    tree = convert(Node{T}, tree)
+    X = randn(MersenneTwister(0), T, 1, 100) * 100.0f0
+    output, flag = eval_tree_array(tree, X, options)
+    @test !flag
 
-# Creating a NaN/Inf via division by constant zero.
-tree = cos(Node("x1") / 0.0f0)
-output, flag = eval_tree_array(tree, X, options)
-@test !flag
+    # Creating a NaN/Inf via division by constant zero.
+    tree = cos(Node("x1") / 0.0f0)
+    tree = convert(Node{T}, tree)
+    output, flag = eval_tree_array(tree, X, options)
+    @test !flag
 
-# Having a NaN/Inf constants:
-tree = cos(Node("x1") + Inf)
-output, flag = eval_tree_array(tree, X, options)
-@test !flag
-tree = cos(Node("x1") + NaN)
-output, flag = eval_tree_array(tree, X, options)
-@test !flag
+    # Having a NaN/Inf constants:
+    tree = cos(Node("x1") + Inf)
+    tree = convert(Node{T}, tree)
+    output, flag = eval_tree_array(tree, X, options)
+    @test !flag
+    tree = cos(Node("x1") + NaN)
+    tree = convert(Node{T}, tree)
+    output, flag = eval_tree_array(tree, X, options)
+    @test !flag
+end
 
 println("Passed.")
