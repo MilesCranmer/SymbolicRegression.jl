0782359f737faea783d912c046c7b51095356824
MilesCranmer <miles.cranmer@gmail.com>
2022-08-30 15:47:34 -0400

    Add back equation3, but skip Float16 test

--- test/test_derivatives.jl
+++ test/test_derivatives.jl
@@ -20,6 +20,19 @@ custom_cos(x::Node)::Node = x.constant ? Node(custom_cos(x.val)) : Node(1, x)
 
 equation1(x1, x2, x3) = x1 + x2 + x3 + 3.2
 equation2(x1, x2, x3) = pow_abs2(x1, x2) + x3 + custom_cos(1.0 + x3) + 3.0 / x1
+function equation3(x1, x2, x3)
+    return (
+        ((x2 + x2) * ((-0.5982493 / pow_abs2(x1, x2)) / -0.54734415)) + (
+            sin(
+                custom_cos(
+                    sin(1.2926733 - 1.6606787) / sin(
+                        ((0.14577048 * x1) + ((0.111149654 + x1) - -0.8298334)) - -1.2071426
+                    ),
+                ) * (custom_cos(x3 - 2.3201916) + ((x1 - (x1 * x2)) / x2)),
+            ) / (0.14854191 - ((custom_cos(x2) * -1.6047639) - 0.023943262))
+        )
+    )
+end
 
 nx1 = Node("x1")
 nx2 = Node("x2")
@@ -31,25 +44,27 @@ function array_test(ar1, ar2; rtol=0.1)
     return isapprox(ar1, ar2; rtol=rtol)
 end
 
-options = Options(;
-    binary_operators=(+, *, -, /, pow_abs2),
-    unary_operators=(custom_cos, exp, sin),
-    enable_autodiff=true,
-)
-
 
 for type in [Float16, Float32, Float64]
     println("Testing derivatives with respect to variables, with type=$(type).")
     rng = MersenneTwister(seed)
     nfeatures = 3
-    N = 10
+    N = 100
 
-    X = rand(rng, type, nfeatures, N) .* 5
+    X = rand(rng, type, nfeatures, N) * 5
 
+    options = Options(;
+        binary_operators=(+, *, -, /, pow_abs2),
+        unary_operators=(custom_cos, exp, sin),
+        enable_autodiff=true,
+    )
 
-
-    for j in 1:2
-        equation = [equation1, equation2][j]
+    for j in 1:3
+        equation = [equation1, equation2, equation3][j]
+        if type == Float16 && j == 3
+            # Numerical precision hurts this comparison too much
+            continue
+        end
 
         tree = convert(Node{type}, equation(nx1, nx2, nx3))
         predicted_output = eval_tree_array(tree, X, options)[1]
@@ -126,7 +141,7 @@ options = Options(;
     unary_operators=(custom_cos, exp, sin),
     enable_autodiff=true,
 )
-tree = equation2(nx1, nx2, nx3)
+tree = equation3(nx1, nx2, nx3)
 
 """Check whether the ordering of constant_list is the same as the ordering of node_index."""
 function check_tree(tree::Node, node_index::NodeIndex, constant_list::AbstractVector)
