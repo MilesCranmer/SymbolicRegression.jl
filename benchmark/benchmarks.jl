72b670b553ffb4be8bc5b0b6d0d61e2a0512d5b4
MilesCranmer <miles.cranmer@gmail.com>
2022-09-06 19:39:22 -0400

    Fix constructors in benchmarks file

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -8,13 +8,13 @@ tree = Node(
     2,
     Node(
         1,
-        Node(3, Node(1, Node(1.0f0), Node(2)), Node(2, Node(-1.0f0))),
-        Node(1, Node(3), Node(4)),
+        Node(3, Node(1, Node(;val=1.0f0), Node(;feature=2)), Node(2, Node(;val=-1.0f0))),
+        Node(1, Node(;feature=3), Node(;feature=4)),
     ),
     Node(
         4,
-        Node(3, Node(1, Node(1.0f0), Node(2)), Node(2, Node(-1.0f0))),
-        Node(1, Node(3), Node(4)),
+        Node(3, Node(1, Node(;val=1.0f0), Node(;feature=2)), Node(2, Node(;val=-1.0f0))),
+        Node(1, Node(;feature=3), Node(;feature=4)),
     ),
 )
 
@@ -23,6 +23,7 @@ _X = randn(MersenneTwister(0), Float32, 5, 1000)
 SUITE["evaluation"] = BenchmarkGroup()
 for T in [Float32, Float64, BigFloat]
     X = map(x -> convert(T, x), _X)
+    tree = convert(Node{T}, tree)
     f = eval_tree_array
     SUITE["evaluation"][string(T)] = @benchmarkable ($f)(tree, $X, options)
 end
