ba6398033d1ac5b033c031cf36ece489b7b8b542
Johan Blåbäck <johan_bluecreek@riseup.net>
2022-09-08 17:35:51 +0200

    Adding tests to printing verifying the printed names of the "safe_" operators
    There are now tests for the new safe operators that verifies that the
    "safe_" part of the name is removed when printed to the user. The tests
    are not only for the unary operators but the binary "safe_pow" as well.

--- test/test_print.jl
+++ test/test_print.jl
@@ -31,3 +31,19 @@ EquationSearch(
 s = repr(tree)
 true_s = "((sin(cos(sin(cos(v1) * v3) * 3.0) * -0.5) + 2.0) * 5.0)"
 @test s == true_s
+
+for unaop in [safe_log, safe_log2, safe_log10, safe_log1p, safe_sqrt, safe_acosh]
+    opts = Options(;
+        default_params..., binary_operators=(+, *, /, -), unary_operators=(unaop,)
+    )
+    minitree = Node(1, Node("x1"))
+    @test string_tree(minitree, opts) == replace(string(unaop), "safe_" => "") * "(x1)"
+end
+
+for binop in [safe_pow, ^]
+    opts = Options(;
+        default_params..., binary_operators=(+, *, /, -, binop), unary_operators=(cos,)
+    )
+    minitree = Node(5, Node("x1"), Node("x2"))
+    @test string_tree(minitree, opts) == "(x1 ^ x2)"
+end
