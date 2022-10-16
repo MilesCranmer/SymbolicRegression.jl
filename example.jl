ed317bd1e1374933c4d4154025cc0586a44a7354
MilesCranmer <miles.cranmer@gmail.com>
2022-10-15 22:35:25 -0400

    Remove `SymbolicUtils.simplify` from CI

--- example.jl
+++ example.jl
@@ -11,9 +11,12 @@ hall_of_fame = EquationSearch(X, y; niterations=40, options=options, numprocs=4)
 
 dominating = calculate_pareto_frontier(X, y, hall_of_fame, options)
 
-eqn = node_to_symbolic(dominating[end].tree, options)
-println(simplify(eqn * 5 + 3))
+trees = [member.tree for member in dominating]
 
+tree = trees[end]
+output, did_succeed = eval_tree_array(tree, X, options)
+
+eqn = node_to_symbolic(dominating[end].tree, options)
 println("Complexity\tMSE\tEquation")
 
 for member in dominating
