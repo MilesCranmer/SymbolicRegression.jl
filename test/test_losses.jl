2ceb369fb457186dd4f0b95142a32d7ec68acea5
MilesCranmer <miles.cranmer@gmail.com>
2022-10-23 12:31:06 -0400

    Fix loss tests

--- test/test_losses.jl
+++ test/test_losses.jl
@@ -22,6 +22,7 @@ for (loss_fnc, evaluator) in [(L1DistLoss(), testl1), (customloss, customloss)]
     x = randn(MersenneTwister(0), Float32, 100)
     y = randn(MersenneTwister(1), Float32, 100)
     w = abs.(randn(MersenneTwister(2), Float32, 100))
-    @test abs(_loss(x, y, options) - sum(evaluator.(x, y)) / length(x)) < 1e-6
-    @test abs(_weighted_loss(x, y, w, options) - sum(evaluator.(x, y, w)) / sum(w)) < 1e-6
+    @test abs(_loss(x, y, options.loss) - sum(evaluator.(x, y)) / length(x)) < 1e-6
+    @test abs(_weighted_loss(x, y, w, options.loss) - sum(evaluator.(x, y, w)) / sum(w)) <
+        1e-6
 end
