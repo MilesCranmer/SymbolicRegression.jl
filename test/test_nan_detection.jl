c276a084ef18dd8ed4819b00d1059a6fbba9cd04
MilesCranmer <miles.cranmer@gmail.com>
2022-10-28 15:15:22 -0400

    Fix evocation of turbo

--- test/test_nan_detection.jl
+++ test/test_nan_detection.jl
@@ -6,28 +6,30 @@ for T in [Float16, Float32, Float64], turbo in [true, false]
     T == Float16 && turbo && continue
     local options, tree, X
 
-    options = Options(; binary_operators=(+, *, /, -), unary_operators=(cos, sin, exp))
+    options = Options(;
+        binary_operators=(+, *, /, -), unary_operators=(cos, sin, exp), turbo=turbo
+    )
     # Creating a NaN via computation.
-    tree = exp(exp(exp(exp(Node("x1") + 1))))
+    tree = exp(exp(exp(exp(Node(T; feature=1) + 1))))
     tree = convert(Node{T}, tree)
-    X = ones(T, 1, 10) .* 100
-    output, flag = eval_tree_array(tree, X, options; turbo=turbo)
+    X = fill(T(100), 1, 10)
+    output, flag = eval_tree_array(tree, X, options)
     @test !flag
 
     # Creating a NaN/Inf via division by constant zero.
-    tree = cos(Node("x1") / 0.0f0)
+    tree = cos(Node(T; feature=1) / zero(T))
     tree = convert(Node{T}, tree)
-    output, flag = eval_tree_array(tree, X, options; turbo=turbo)
+    output, flag = eval_tree_array(tree, X, options)
     @test !flag
 
     # Having a NaN/Inf constants:
-    tree = cos(Node("x1") + T(Inf))
+    tree = cos(Node(T; feature=1) + T(Inf))
     tree = convert(Node{T}, tree)
-    output, flag = eval_tree_array(tree, X, options; turbo=turbo)
+    output, flag = eval_tree_array(tree, X, options)
     @test !flag
-    tree = cos(Node("x1") + T(NaN))
+    tree = cos(Node(T; feature=1) + T(NaN))
     tree = convert(Node{T}, tree)
-    output, flag = eval_tree_array(tree, X, options; turbo=turbo)
+    output, flag = eval_tree_array(tree, X, options)
     @test !flag
 end
 
