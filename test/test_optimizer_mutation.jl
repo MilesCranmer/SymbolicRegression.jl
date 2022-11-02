da161793ee42c7c9dc832efe40223580567cc212
MilesCranmer <miles.cranmer@gmail.com>
2022-11-02 19:45:21 -0400

    Add test of mutation operator that optimizes

---
+++ test/test_optimizer_mutation.jl
@@ -0,0 +1,41 @@
+using SymbolicRegression
+using SymbolicRegression: SymbolicRegression
+using SymbolicRegression: Dataset, RunningSearchStatistics, RecordType
+using Optim: Optim
+import SymbolicRegression.MutateModule: next_generation
+import DynamicExpressions: get_constants
+using Test
+
+mutation_weights = MutationWeights(; optimize=Inf)
+options = Options(;
+    binary_operators=(+, -, *),
+    unary_operators=(cos,),
+    mutation_weights=mutation_weights,
+    optimizer_options=Optim.Options(),
+)
+
+X = randn(5, 100)
+y = cos.(X[1, :] .* 2.1 .- 0.8) .+ X[2, :] .^ 2
+dataset = Dataset(X, y)
+
+x1 = Node(; feature=1)
+x2 = Node(; feature=2)
+tree = cos(x1 * 1.9 - 0.2) + x2 * x2
+
+member = PopMember(dataset, tree, options; deterministic=false)
+temperature = 1.0
+maxsize = 20
+
+new_member, _, _ = next_generation(
+    dataset,
+    member,
+    temperature,
+    maxsize,
+    RunningSearchStatistics(; options=options),
+    options;
+    tmp_recorder=RecordType(),
+)
+
+resultant_constants = get_constants(new_member.tree)
+@test resultant_constants[1] ≈ 2.1 atol = 1e-3
+@test resultant_constants[2] ≈ 0.8 atol = 1e-3
