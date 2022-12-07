69d8910a63d602ab10e7800a632095882cfe3f73
MilesCranmer <miles.cranmer@gmail.com>
2022-12-07 09:11:40 -0500

    Start deprecating `fast_cycle`

--- src/RegularizedEvolution.jl
+++ src/RegularizedEvolution.jl
@@ -29,36 +29,11 @@ function reg_evol_cycle(
 
     num_evals = 0.0
 
-    if options.fast_cycle
-
-        # These options are not implemented for fast_cycle:
-        @recorder error(
-            "You cannot have the recorder and fast_cycle set to true at the same time!"
-        )
-        @assert options.prob_pick_first == 1.0
-        @assert options.crossover_probability == 0.0
-
-        shuffle!(pop.members)
-        n_evol_cycles = round(Int, pop.n / options.tournament_selection_n)
-        babies = Array{PopMember}(undef, n_evol_cycles)
-        accepted = Array{Bool}(undef, n_evol_cycles)
-        array_num_evals = Array{Float64}(undef, n_evol_cycles)
-
-        # Iterate each tournament_selection_n-member sub-sample
-        Threads.@threads for i in 1:n_evol_cycles
-            best_score = Inf
-            best_idx = 1 + (i - 1) * options.tournament_selection_n
-            # Calculate best member of the subsample:
-            for sub_i in
-                (1 + (i - 1) * options.tournament_selection_n):(i * options.tournament_selection_n)
-                if pop.members[sub_i].score < best_score
-                    best_score = pop.members[sub_i].score
-                    best_idx = sub_i
-                end
-            end
-            allstar = pop.members[best_idx]
+    for i in 1:round(Int, pop.n / options.tournament_selection_n)
+        if rand() > options.crossover_probability
+            allstar = best_of_sample(pop, running_search_statistics, options)
             mutation_recorder = RecordType()
-            babies[i], accepted[i], array_num_evals[i] = next_generation(
+            baby, mutation_accepted, tmp_num_evals = next_generation(
                 dataset,
                 allstar,
                 temperature,
@@ -67,91 +42,66 @@ function reg_evol_cycle(
                 options;
                 tmp_recorder=mutation_recorder,
             )
-        end
-        num_evals = sum(array_num_evals)
+            num_evals += tmp_num_evals
 
-        # Replace the n_evol_cycles-oldest members of each population
-        for i in 1:n_evol_cycles
+            if !mutation_accepted && options.skip_mutation_failures
+                # Skip this mutation rather than replacing oldest member with unchanged member
+                continue
+            end
+
             oldest = argmin([pop.members[member].birth for member in 1:(pop.n)])
-            if accepted[i] || !options.skip_mutation_failures
-                pop.members[oldest] = babies[i]
-            end
-        end
-    else
-        for i in 1:round(Int, pop.n / options.tournament_selection_n)
-            if rand() > options.crossover_probability
-                allstar = best_of_sample(pop, running_search_statistics, options)
-                mutation_recorder = RecordType()
-                baby, mutation_accepted, tmp_num_evals = next_generation(
-                    dataset,
-                    allstar,
-                    temperature,
-                    curmaxsize,
-                    running_search_statistics,
-                    options;
-                    tmp_recorder=mutation_recorder,
-                )
-                num_evals += tmp_num_evals
 
-                if !mutation_accepted && options.skip_mutation_failures
-                    # Skip this mutation rather than replacing oldest member with unchanged member
-                    continue
+            @recorder begin
+                if !haskey(record, "mutations")
+                    record["mutations"] = RecordType()
                 end
-
-                oldest = argmin([pop.members[member].birth for member in 1:(pop.n)])
-
-                @recorder begin
-                    if !haskey(record, "mutations")
-                        record["mutations"] = RecordType()
+                for member in [allstar, baby, pop.members[oldest]]
+                    if !haskey(record["mutations"], "$(member.ref)")
+                        record["mutations"]["$(member.ref)"] = RecordType(
+                            "events" => Vector{RecordType}(),
+                            "tree" => string_tree(member.tree, options.operators),
+                            "score" => member.score,
+                            "loss" => member.loss,
+                            "parent" => member.parent,
+                        )
                     end
-                    for member in [allstar, baby, pop.members[oldest]]
-                        if !haskey(record["mutations"], "$(member.ref)")
-                            record["mutations"]["$(member.ref)"] = RecordType(
-                                "events" => Vector{RecordType}(),
-                                "tree" => string_tree(member.tree, options.operators),
-                                "score" => member.score,
-                                "loss" => member.loss,
-                                "parent" => member.parent,
-                            )
-                        end
-                    end
-                    mutate_event = RecordType(
-                        "type" => "mutate",
-                        "time" => time(),
-                        "child" => baby.ref,
-                        "mutation" => mutation_recorder,
-                    )
-                    death_event = RecordType("type" => "death", "time" => time())
-
-                    # Put in random key rather than vector; otherwise there are collisions!
-                    push!(record["mutations"]["$(allstar.ref)"]["events"], mutate_event)
-                    push!(
-                        record["mutations"]["$(pop.members[oldest].ref)"]["events"],
-                        death_event,
-                    )
                 end
-
-                pop.members[oldest] = baby
-
-            else # Crossover
-                allstar1 = best_of_sample(pop, running_search_statistics, options)
-                allstar2 = best_of_sample(pop, running_search_statistics, options)
-
-                baby1, baby2, crossover_accepted, tmp_num_evals = crossover_generation(
-                    allstar1, allstar2, dataset, curmaxsize, options
+                mutate_event = RecordType(
+                    "type" => "mutate",
+                    "time" => time(),
+                    "child" => baby.ref,
+                    "mutation" => mutation_recorder,
                 )
-                num_evals += tmp_num_evals
+                death_event = RecordType("type" => "death", "time" => time())
 
-                if !crossover_accepted && options.skip_mutation_failures
-                    continue
-                end
-
-                # Replace old members with new ones:
-                oldest = argmin([pop.members[member].birth for member in 1:(pop.n)])
-                pop.members[oldest] = baby1
-                oldest = argmin([pop.members[member].birth for member in 1:(pop.n)])
-                pop.members[oldest] = baby2
+                # Put in random key rather than vector; otherwise there are collisions!
+                push!(record["mutations"]["$(allstar.ref)"]["events"], mutate_event)
+                push!(
+                    record["mutations"]["$(pop.members[oldest].ref)"]["events"],
+                    death_event,
+                )
             end
+
+            pop.members[oldest] = baby
+
+        else # Crossover
+            allstar1 = best_of_sample(pop, running_search_statistics, options)
+            allstar2 = best_of_sample(pop, running_search_statistics, options)
+
+            baby1, baby2, crossover_accepted, tmp_num_evals = crossover_generation(
+                allstar1, allstar2, dataset, curmaxsize, options
+            )
+            num_evals += tmp_num_evals
+
+            if !crossover_accepted && options.skip_mutation_failures
+                continue
+            end
+
+            # Replace old members with new ones:
+            oldest = argmin([pop.members[member].birth for member in 1:(pop.n)])
+            pop.members[oldest] = baby1
+            oldest = argmin([pop.members[member].birth for member in 1:(pop.n)])
+            pop.members[oldest] = baby2
         end
     end
 
