c93a6e15251e4f20e4faa3c2f6ac201ff7c9dc6b
MilesCranmer <miles.cranmer@gmail.com>
2023-03-24 21:18:59 -0400

    Turn on every test again

--- test/full.jl
+++ test/full.jl
@@ -4,46 +4,46 @@ using SafeTestsets
 using SymbolicRegression: string_tree
 using Random
 
-# @safetestset "Test mixed settings." begin
-#     include("test_mixed.jl")
-# end
+@safetestset "Test mixed settings." begin
+    include("test_mixed.jl")
+end
 
-# @safetestset "Testing fast-cycle and custom variable names, with mutations" begin
-#     include("test_fast_cycle.jl")
-# end
+@safetestset "Testing fast-cycle and custom variable names, with mutations" begin
+    include("test_fast_cycle.jl")
+end
 
-# @safetestset "Testing whether we can stop based on clock time." begin
-#     include("test_stop_on_clock.jl")
-# end
+@safetestset "Testing whether we can stop based on clock time." begin
+    include("test_stop_on_clock.jl")
+end
 
-# @safetestset "Running README example." begin
-#     include("../example.jl")
-# end
+@safetestset "Running README example." begin
+    include("../example.jl")
+end
 
-# @safetestset "Testing whether the recorder works." begin
-#     include("test_recorder.jl")
-# end
+@safetestset "Testing whether the recorder works." begin
+    include("test_recorder.jl")
+end
 
-# @safetestset "Testing whether deterministic mode works." begin
-#     include("test_deterministic.jl")
-# end
+@safetestset "Testing whether deterministic mode works." begin
+    include("test_deterministic.jl")
+end
 
 @safetestset "Testing whether early stop criteria works." begin
     include("test_early_stop.jl")
 end
 
-# @testset "Testing whether we can move operators to workers." begin
-#     include("test_custom_operators_multiprocessing.jl")
-# end
+@testset "Testing whether we can move operators to workers." begin
+    include("test_custom_operators_multiprocessing.jl")
+end
 
 # @testset "Test whether the precompilation script works." begin
 #     include("test_precompilation.jl")
 # end
 
-# @testset "Test whether custom objectives work." begin
-#     include("test_custom_objectives.jl")
-# end
+@testset "Test whether custom objectives work." begin
+    include("test_custom_objectives.jl")
+end
 
-# @testset "Test abstract numbers" begin
-#     include("test_abstract_numbers.jl")
-# end
+@testset "Test abstract numbers" begin
+    include("test_abstract_numbers.jl")
+end
