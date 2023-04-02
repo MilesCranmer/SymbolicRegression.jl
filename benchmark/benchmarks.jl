164d6aa9727141791f95509c27c70010d8535616
MilesCranmer <miles.cranmer@gmail.com>
2023-04-02 00:58:42 -0400

    Clean up benchmark file

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,69 +1,94 @@
 using SymbolicRegression, BenchmarkTools, Random
 using SymbolicRegression: eval_tree_array
 
-const SUITE = BenchmarkGroup()
-
-const simple_tree = Node(
-    2,
-    Node(
-        1,
-        Node(3, Node(1, Node(; val=1.0f0), Node(; feature=2)), Node(2, Node(; val=-1.0f0))),
-        Node(1, Node(; feature=3), Node(; feature=4)),
-    ),
-    Node(
-        4,
-        Node(3, Node(1, Node(; val=1.0f0), Node(; feature=2)), Node(2, Node(; val=-1.0f0))),
-        Node(1, Node(; feature=3), Node(; feature=4)),
-    ),
-)
-
-SUITE["evaluation"] = BenchmarkGroup()
-for T in (Float32, Float64, BigFloat)
-    local options, tree, X
+function create_evaluation_benchmark()
+    suite = BenchmarkGroup()
     options = Options(;
         binary_operators=(+, -, /, *),
         unary_operators=(cos, exp),
         verbosity=0,
         progress=false,
+        define_helper_functions=false,
+        save_to_file=false,
     )
-    X = T.(randn(MersenneTwister(0), Float32, 5, 1000))
-    tree = convert(Node{T}, copy_node(simple_tree))
-    SUITE["evaluation"][string(T)] = @benchmarkable eval_tree_array($tree, $X, options) evals =
-        1000
-end
-
-SUITE["search"] = BenchmarkGroup()
-for parallelism in (:serial,)
-    # TODO: Add determinism for other parallelisms
-    function f(turbo)
-        for seed in 1:3
-            extra_kws = hasfield(Options, :turbo) ? (turbo=turbo,) : NamedTuple()
-            local options, X, y, niterations
-            niterations = 100
-            options = Options(;
-                binary_operators=(+, -, /, *),
-                unary_operators=(exp, abs),
-                maxsize=30,
-                verbosity=0,
-                progress=false,
-                deterministic=true,
-                seed=seed,
-                extra_kws...,
-            )
-            X = rand(MersenneTwister(seed), Float32, 5, 1000) .* 10 .- 5
-
-            # We create an equation that cannot be found exactly, so the search
-            # is more realistic.
-            y = @. cos(2.13 * X[1, :]) + 0.5 * X[2, :] * abs(X[3, :])^0.9 -
-                0.3 * abs(X[4, :])^1.5
-            # Also, add some noise:
-            y .+= 0.1 .* randn(MersenneTwister(seed + 1), Float32, length(y))
-
-            X = Float32.(X)
-            y = Float32.(y)
-
-            EquationSearch(X, y; options, parallelism, niterations)
-        end
+    simple_tree = Node(
+        2,
+        Node(
+            1,
+            Node(
+                3,
+                Node(1, Node(; val=1.0f0), Node(; feature=2)),
+                Node(2, Node(; val=-1.0f0)),
+            ),
+            Node(1, Node(; feature=3), Node(; feature=4)),
+        ),
+        Node(
+            4,
+            Node(
+                3,
+                Node(1, Node(; val=1.0f0), Node(; feature=2)),
+                Node(2, Node(; val=-1.0f0)),
+            ),
+            Node(1, Node(; feature=3), Node(; feature=4)),
+        ),
+    )
+    for T in (Float32, Float64, BigFloat)
+        local X, tree
+        X = T.(randn(MersenneTwister(0), Float32, 5, 1000))
+        tree = convert(Node{T}, copy_node(simple_tree))
+        suite[string(T)] = @benchmarkable eval_tree_array($tree, $X, $options) evals = 1000
     end
-    SUITE["search"][string(parallelism)] = @benchmarkable ($f)(true) evals = 10
+    return suite
 end
+
+function create_search_benchmark()
+    suite = BenchmarkGroup()
+
+    n = 1000
+    T = Float32
+
+    extra_kws = hasfield(Options, :turbo) ? (turbo=true,) : NamedTuple()
+    option_kws = (;
+        binary_operators=(+, -, /, *),
+        unary_operators=(exp, abs),
+        maxsize=30,
+        verbosity=0,
+        progress=false,
+        deterministic=true,
+        define_helper_functions=false,
+        save_to_file=false,
+        extra_kws...,
+    )
+    seeds = 1:3
+    niterations = 100
+    # We create an equation that cannot be found exactly, so the search
+    # is more realistic.
+    eqn(x) = Float32(cos(2.13 * x[1]) + 0.5 * x[2] * abs(x[3])^0.9 - 0.3 * abs(x[4])^1.5)
+    all_options = [Options(; seed=seed, option_kws...) for seed in seeds]
+    all_X = [rand(MersenneTwister(seed), T, 5, n) .* 10 .- 5 for seed in seeds]
+    all_y = [
+        [eqn(x) for x in eachcol(X)] .+ 0.1f0 .* randn(MersenneTwister(seed + 1), T, n) for
+        (X, seed) in zip(all_X, seeds)
+    ]
+
+    for parallelism in (:serial,)
+        # TODO: Add determinism for other parallelisms
+        function f()
+            for (options, X, y) in zip(all_options, all_X, all_y)
+                EquationSearch(X, y; options, parallelism, niterations)
+            end
+        end
+        suite[string(parallelism)] = @benchmarkable ($f)() evals = 10
+    end
+    return suite
+end
+
+function create_benchmark()
+    suite = BenchmarkGroup()
+    suite["evaluation"] = create_evaluation_benchmark()
+    suite["search"] = create_search_benchmark()
+
+    return suite
+end
+
+const SUITE = create_benchmark()
