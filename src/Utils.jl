9c55d276f569efb35b855f254ab33f57deb87943
MilesCranmer <miles.cranmer@gmail.com>
2023-05-09 06:16:06 -0400

    Create fast bottomk and argmin
    - See https://discourse.julialang.org/t/what-is-julias-maxk-matlab-that-returns-the-indice-of-top-k-largest-values/14100/13 and https://discourse.julialang.org/t/why-is-minimum-so-much-faster-than-argmin/66814/9

--- src/Utils.jl
+++ src/Utils.jl
@@ -2,6 +2,8 @@
 module UtilsModule
 
 import Printf: @printf
+import StaticArrays: MVector
+import LoopVectorization: @turbo
 
 function debug(verbosity, string...)
     if verbosity > 0
@@ -50,7 +52,49 @@ function recursive_merge(x...)
     return x[end]
 end
 
-const max_ops = 1024
-const vals = ntuple(i -> Val(i), max_ops)
+const max_ops = 8192
+const vals = ntuple(Val, max_ops)
+
+"""Return the bottom k elements of x, and their indices."""
+bottomk_fast(x, k) = _bottomk_dispatch(x, vals[k])
+
+function _bottomk_dispatch(x::AbstractVector{T}, ::Val{k}) where {T,k}
+    @assert k >= 2
+    indmin = MVector{k}(ntuple(_ -> 0, k))
+    minval = MVector{k}(ntuple(_ -> typemax(T), k))
+    _bottomk!(x, minval, indmin)
+    return [minval...], [indmin...]
+end
+function _bottomk!(x, minval, indmin)
+    @inbounds @fastmath for i in eachindex(x)
+        new_min = x[i] < minval[end]
+        if new_min
+            minval[end] = x[i]
+            indmin[end] = i
+            for ki in length(minval):-1:2
+                need_swap = minval[ki] < minval[ki - 1]
+                if need_swap
+                    minval[ki], minval[ki - 1] = minval[ki - 1], minval[ki]
+                    indmin[ki], indmin[ki - 1] = indmin[ki - 1], indmin[ki]
+                end
+            end
+        end
+    end
+    return nothing
+end
+
+# Thanks Chris Elrod
+# https://discourse.julialang.org/t/why-is-minimum-so-much-faster-than-argmin/66814/9
+function findmin_fast(x)
+    indmin = 0
+    minval = typemax(eltype(x))
+    @turbo for i in eachindex(x)
+        newmin = x[i] < minval
+        minval = newmin ? x[i] : minval
+        indmin = newmin ? i : indmin
+    end
+    return minval, indmin
+end
+argmin_fast(x) = findmin_fast(x)[2]
 
 end
