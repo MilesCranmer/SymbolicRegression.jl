d4f8f461a02247e39910e1ebd5c337468255861f
MilesCranmer <miles.cranmer@gmail.com>
2023-06-05 05:11:28 -0400

    Clean up printing

--- ext/MicroUnitful.jl
+++ ext/MicroUnitful.jl
@@ -23,7 +23,7 @@ struct Quantity{T}
     Quantity(x, data::COMPAT_D_TYPE, valid::Bool) = new{typeof(x)}(x, Dimensions(data), valid)
 end
 
-Base.show(io::IO, d::Dimensions) = foreach(k -> (print(io, k); pretty_print_exponent(io, d[k]); print(io, " ")), keys(d))
+Base.show(io::IO, d::Dimensions) = foreach(k -> d[k] != 0 ? (print(io, k); pretty_print_exponent(io, d[k]); print(io, " ")) : print(io, ""), keys(d))
 Base.show(io::IO, q::Quantity) = q.valid ? print(io, q.val, " ", q.dimensions) : print(io, "INVALID")
 tryround(x::Rational{Int}) = isinteger(x) ? round(Int, x) : x
 pretty_print_exponent(io::IO, x::Rational{Int}) = (x >= 0 && isinteger(x)) ? print(io, "^", round(Int, x)) : print(io, "^(", tryround(x), ")")
@@ -34,6 +34,8 @@ Base.values(d::Dimensions) = values(d.data)
 Base.iszero(d::Dimensions) = all(iszero, values(d))
 Base.getindex(d::Dimensions, k::Symbol) = get(d.data, k, zero(Rational{Int}))
 Base.:(==)(l::Dimensions, r::Dimensions) = all(k -> (l[k] == r[k]), union(keys(l), keys(r)))
+Base.convert(::Type{T}, q::Quantity) where {T<:Real} = q.valid ? (iszero(q.dimensions) ? convert(T, q.val) : throw(error("Quantity $(q) has dimensions!"))) : throw(error("Quantity $(q) is invalid!"))
+Base.float(q::Quantity{T}) where {T<:AbstractFloat} = convert(T, q)
 
 Base.:*(l::Dimensions, r::Dimensions) = Dimensions(D_TYPE([(k, l[k] + r[k]) for k in union(keys(l.data), keys(r.data))]))
 Base.:/(l::Dimensions, r::Dimensions) = Dimensions(D_TYPE([(k, l[k] - r[k]) for k in union(keys(l.data), keys(r.data))]))
