63f93a71ca53880ca30e002c7dae791cded1bc6e
MilesCranmer <miles.cranmer@gmail.com>
2023-06-14 15:31:06 -0400

    Fix parallel coveralls build

--- coverage.jl
+++ coverage.jl
@@ -1,6 +1,7 @@
 using Coverage
 # process '*.cov' files
 coverage = process_folder() # defaults to src/; alternatively, supply the folder name as argument
+push!(coverage, process_folder("ext")...)
 
 LCOV.writefile("lcov.info", coverage)
 
@@ -8,7 +9,7 @@ LCOV.writefile("lcov.info", coverage)
 coverage = merge_coverage_counts(
     coverage,
     filter!(
-        let prefixes = (joinpath(pwd(), "src", ""),)
+        let prefixes = (joinpath(pwd(), "src", ""), joinpath(pwd(), "ext", ""))
             c -> any(p -> startswith(c.filename, p), prefixes)
         end,
         LCOV.readfolder("test"),
