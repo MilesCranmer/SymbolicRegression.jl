50bfaece325e82d40cdbd51fa72dc2202133c166
MilesCranmer <miles.cranmer@gmail.com>
2023-07-04 05:53:00 -0400

    Add variable names test

--- test/test_mlj.jl
+++ test/test_mlj.jl
@@ -1,8 +1,9 @@
 import SymbolicRegression: SRRegressor, MultitargetSRRegressor
 import MLJTestInterface as MTI
+import MLJBase: machine, fit!, report
 using Test
 
-@testset "generic interface tests" begin
+@testset "Generic interface tests" begin
     failures, summary = MTI.test(
         [SRRegressor], MTI.make_regression()...; mod=@__MODULE__, verbosity=0, throw=true
     )
@@ -15,4 +16,28 @@ using Test
         [MultitargetSRRegressor], X, Y; mod=@__MODULE__, verbosity=0, throw=true
     )
     @test isempty(failures)
+end
+
+@testset "Variable names" begin
+    @testset "Single outputs" begin
+        X = (a=rand(32), b=rand(32))
+        y = X.a .^ 2.1
+        model = SRRegressor(; niterations=10)
+        mach = machine(model, X, y)
+        fit!(mach)
+        rep = report(mach)
+        @test occursin("a", rep.equation_strings[rep.best_idx])
+    end
+
+    @testset "Multiple outputs" begin
+        X = (a=rand(32), b=rand(32))
+        y = X.a .^ 2.1
+        model = MultitargetSRRegressor(; niterations=10)
+        mach = machine(model, X, reduce(hcat, [reshape(y, :, 1) for i in 1:3]))
+        fit!(mach)
+        rep = report(mach)
+        @test all(
+            eq -> occursin("a", eq), [rep.equation_strings[i][rep.best_idx[i]] for i in 1:3]
+        )
+    end
 end
