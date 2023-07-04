8265113b3773dd68ba687817972d31f537d3a7bb
MilesCranmer <miles.cranmer@gmail.com>
2023-07-04 02:52:01 -0400

    Test MultiSRRegressor

--- test/test_mlj.jl
+++ test/test_mlj.jl
@@ -1,10 +1,18 @@
-import SymbolicRegression: SRRegressor
+import SymbolicRegression: SRRegressor, MultiSRRegressor
 import MLJTestInterface as MTI
 using Test
 
 @testset "generic interface tests" begin
     failures, summary = MTI.test(
         [SRRegressor], MTI.make_regression()...; mod=@__MODULE__, verbosity=0, throw=true
+    )
+    @test isempty(failures)
+
+    X = randn(100, 3)
+    Y = @. cos(X^2) * 3.2 - 0.5
+    (X, Y) = MTI.table.((X, Y))
+    failures, summary = MTI.test(
+        [MultiSRRegressor], X, Y; mod=@__MODULE__, verbosity=0, throw=true
     )
     @test isempty(failures)
 end
