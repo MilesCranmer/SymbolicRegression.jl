d54f5157dde58ee638be630f3ab17e52a514ce05
MilesCranmer <miles.cranmer@gmail.com>
2023-07-04 00:30:29 -0400

    Formatting

--- test/test_mlj.jl
+++ test/test_mlj.jl
@@ -4,11 +4,7 @@ using Test
 
 @testset "generic interface tests" begin
     failures, summary = MTI.test(
-        [SRRegressor],
-        MTI.make_regression()...;
-        mod=@__MODULE__,
-        verbosity=0,
-        throw=true,
+        [SRRegressor], MTI.make_regression()...; mod=@__MODULE__, verbosity=0, throw=true
     )
     @test isempty(failures)
 end
