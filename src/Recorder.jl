af428e2df58ec779e581199b70dcefd013dd87a1
MilesCranmer <miles.cranmer@gmail.com>
2023-07-21 16:53:12 -0400

    Fix merge

--- src/Recorder.jl
+++ src/Recorder.jl
@@ -2,7 +2,7 @@ module RecorderModule
 
 import ..CoreModule: RecordType, Options
 
-is_recording(::Options{CT,use_recorder} where {CT}) where {use_recorder} = use_recorder
+is_recording(::Options{<:Any,<:Any,use_recorder}) where {use_recorder} = use_recorder
 
 "Assumes that `options` holds the user options::Options"
 macro recorder(ex)
