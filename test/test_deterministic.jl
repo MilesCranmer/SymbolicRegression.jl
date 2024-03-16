d19f15b614a950b72371fbf4bcbc2994dea028c7
MilesCranmer <miles.cranmer@gmail.com>
2024-03-16 23:19:21 +0000

    Fix type inference bug on Julia <1.10

--- test/test_deterministic.jl
+++ test/test_deterministic.jl
@@ -2,6 +2,24 @@ using SymbolicRegression
 using Test
 using Random
 
+macro maybe_inferred(ex)
+    # Only get stable inference on Julia 1.10+
+    return if VERSION >= v"1.10.0-DEV.0"
+        quote
+            @inferred $ex
+        end
+    else
+        quote
+            try
+                # Still want to test for any bugs (JuliaLang/julia#53761)
+                @inferred $ex
+            catch
+                $ex
+            end
+        end
+    end |> esc
+end
+
 begin
     X = 2 .* randn(MersenneTwister(0), Float32, 2, 1000)
     y = 3 * cos.(X[2, :]) + X[1, :] .^ 2 .- 2
@@ -19,7 +37,7 @@ begin
 
     all_outputs = []
     for i in 1:2
-        hall_of_fame = @inferred equation_search(
+        hall_of_fame = @maybe_inferred equation_search(
             X,
             y;
             niterations=5,
