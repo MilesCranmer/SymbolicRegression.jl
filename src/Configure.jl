23e5c291549aae0ee40b11d4f8634bc226ed879c
MilesCranmer <miles.cranmer@gmail.com>
2024-03-23 23:10:07 +0000

    Also add Pkg to workers

--- src/Configure.jl
+++ src/Configure.jl
@@ -220,13 +220,14 @@ function import_module_on_workers(procs, filename::String, options::Options, ver
 
     # Need to import any extension code, if loaded on head node
     relevant_extensions = [
-        :ClusterManagers,
-        :SymbolicUtils,
         :Bumper,
-        :LoopVectorization,
-        :Zygote,
         :CUDA,
+        :ClusterManagers,
         :Enzyme,
+        :LoopVectorization,
+        :Pkg,
+        :SymbolicUtils,
+        :Zygote,
     ]
     filter!(m -> String(m) ∈ loaded_modules_head_worker, relevant_extensions)
     # HACK TODO – this workaround is very fragile. Likely need to submit a bug report
