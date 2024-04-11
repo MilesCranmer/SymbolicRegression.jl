8693249a8309eafc688e112f65e1f4ac12e42122
MilesCranmer <miles.cranmer@gmail.com>
2024-04-11 22:22:59 +0100

    fix: distributed extensions on 1.11

--- src/Configure.jl
+++ src/Configure.jl
@@ -246,7 +246,12 @@ function import_module_on_workers(procs, filename::String, options::Options, ver
     else
         @info "Importing SymbolicRegression on workers as well as extensions $(join(relevant_extensions, ',' * ' '))."
     end
-    @everywhere procs Base.MainInclude.eval($expr)
+    if VERSION < v"1.11.0-alpha1"
+        @everywhere procs Base.MainInclude.eval($expr)
+    else
+        # See https://github.com/JuliaLang/julia/issues/54057
+        @everywhere procs Core.eval(Base.MainInclude, $expr)
+    end
     return verbosity > 0 && @info "Finished!"
 end
 
