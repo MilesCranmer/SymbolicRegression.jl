c6f325ce59020adf3adb8740a292f24c3bd69b73
MilesCranmer <miles.cranmer@gmail.com>
2024-04-28 05:26:24 +0100

    ci: split up test suite into multiple runners

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,25 @@ using SafeTestsets
 using Test
 
 ENV["SYMBOLIC_REGRESSION_TEST"] = "true"
+TEST_SUITE = get(ENV, "SYMBOLIC_REGRESSION_TEST_SUITE", "all")
 
-@safetestset "Aqua tests" begin
-    include("test_aqua.jl")
+if TEST_SUITE in ("all", "integration")
+    @safetestset "Aqua tests" begin
+        include("test_aqua.jl")
+    end
 end
+
 # Trigger extensions:
 using LoopVectorization, Bumper, Zygote
 
-@safetestset "Unit tests" begin
-    include("unittest.jl")
+if TEST_SUITE in ("all", "unit")
+    @safetestset "Unit tests" begin
+        include("unittest.jl")
+    end
 end
-@testset "End to end test" begin
-    include("full.jl")
+
+if TEST_SUITE in ("all", "integration")
+    @eval @testset "End to end test" begin
+        include("full.jl")
+    end
 end
