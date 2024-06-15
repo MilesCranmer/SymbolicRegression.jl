f60161f9c3c4142f24416f27ae8e1d110a83b786
MilesCranmer <miles.cranmer@gmail.com>
2024-06-15 18:47:06 +0100

    test: work around workers issue

--- test/test_custom_operators_multiprocessing.jl
+++ test/test_custom_operators_multiprocessing.jl
@@ -1,17 +1,24 @@
 using SymbolicRegression
 
-_plus(x, y) = x + y
-_mult(x, y) = x * y
-_div(x, y) = x / y
-_min(x, y) = x - y
-_cos(x) = cos(x)
-_exp(x) = exp(x)
+defs = quote
+    _plus(x, y) = x + y
+    _mult(x, y) = x * y
+    _div(x, y) = x / y
+    _min(x, y) = x - y
+    _cos(x) = cos(x)
+    _exp(x) = exp(x)
+    early_stop(loss, c) = ((loss <= 1e-10) && (c <= 10))
+    my_loss(x, y, w) = abs(x - y)^2 * w
+end
+
+# This is needed as workers are initialized in `Core.Main`!
+Core.eval(Core.Main, defs)
+if (@__MODULE__) != Core.Main
+    eval(:(using Main: _plus, _mult, _div, _min, _cos, _exp, early_stop, my_loss))
+end
 
 X = randn(Float32, 5, 100)
 y = _mult.(2, _cos.(X[4, :])) + _mult.(X[1, :], X[1, :])
-early_stop(loss, c) = ((loss <= 1e-10) && (c <= 10))
-
-my_loss(x, y, w) = abs(x - y)^2 * w
 
 options = SymbolicRegression.Options(;
     binary_operators=(_plus, _mult, _div, _min),
