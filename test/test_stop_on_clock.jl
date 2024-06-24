368a1d9764cb82b609444e285e77bc42a0ada6a1
MilesCranmer <miles.cranmer@gmail.com>
2024-06-24 05:22:24 +0100

    test: fix stop on clock test

--- test/test_stop_on_clock.jl
+++ test/test_stop_on_clock.jl
@@ -6,9 +6,9 @@ X = randn(MersenneTwister(0), Float32, 5, 100)
 y = 2 * cos.(X[4, :])
 
 # Ensure is precompiled:
+options = Options(; default_params..., timeout_in_seconds=1)
 equation_search(X, y; niterations=1, options=options, parallelism=:multithreading)
 
-options = Options(; default_params..., timeout_in_seconds=1)
 start_time = time()
 equation_search(X, y; niterations=10000000, options=options, parallelism=:multithreading)
 end_time = time()
