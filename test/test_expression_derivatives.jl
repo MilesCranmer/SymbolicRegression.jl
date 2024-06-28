394e768bf8a5707d776e49def4ee0dc5a2dcae6d
MilesCranmer <miles.cranmer@gmail.com>
2024-06-28 07:10:53 +0100

    test: gradients during optimization

--- test/test_expression_derivatives.jl
+++ test/test_expression_derivatives.jl
@@ -34,3 +34,77 @@
 
     @test δy2 ≈ δy2̂
 end
+
+@testitem "Test derivatives during optimization" tags = [:part1] begin
+    using SymbolicRegression
+    using SymbolicRegression.ConstantOptimizationModule: Evaluator, GradEvaluator
+    using DynamicExpressions
+    using Zygote: Zygote
+    using Random: MersenneTwister
+    using DifferentiationInterface: value_and_gradient
+
+    rng = MersenneTwister(0)
+    X = rand(rng, 2, 32)
+    y = @. X[1, :] * X[1, :] - cos(2.6 * X[2, :])
+    dataset = Dataset(X, y)
+
+    options = Options(;
+        unary_operators=[cos], binary_operators=[+, *, -], autodiff_backend=:Zygote
+    )
+
+    f = Evaluator(dataset, options, nothing)
+    fg! = GradEvaluator(f)
+
+    ex = @parse_expression(
+        x * x - cos(2.5 * y), operators = options.operators, variable_names = [:x, :y]
+    )
+    @test f(ex) isa Float64
+
+    (val, grad) = value_and_gradient(f, options.autodiff_backend, ex)
+    @test val isa Float64
+    @test typeof(grad.tree) <: DynamicExpressions.ChainRulesModule.NodeTangent{
+        Float64,Node{Float64},Vector{Float64}
+    }
+    @test typeof(grad.tree.gradient) <: Vector{Float64}
+end
+
+@testitem "Test derivatives of parametric expression during optimization" tags = [:part1] begin
+    using SymbolicRegression
+    using SymbolicRegression.ConstantOptimizationModule: Evaluator, GradEvaluator
+    using DynamicExpressions
+    using Zygote: Zygote
+    using Random: MersenneTwister
+    using DifferentiationInterface: value_and_gradient
+
+    rng = MersenneTwister(0)
+    X = rand(rng, 2, 32)
+    classes = rand(rng, 1:2, 32)
+    y = @. X[1, :] * X[1, :] - cos(2.6 * X[2, :]) + classes
+    dataset = Dataset(X, y; extra=(; classes))
+
+    options = Options(;
+        unary_operators=[cos], binary_operators=[+, *, -], autodiff_backend=:Zygote
+    )
+
+    f = Evaluator(dataset, options, nothing)
+    fg! = GradEvaluator(f)
+
+    ex = @parse_expression(
+        x * x - cos(2.5 * y) + p1,
+        operators = options.operators,
+        expression_type = ParametricExpression,
+        variable_names = ["x", "y"],
+        extra_metadata = (parameter_names=["p1"], parameters=[0.5 0.2])
+    )
+    @test f(ex) isa Float64
+    (val, grad) = value_and_gradient(f, options.autodiff_backend, ex)
+
+    @test val isa Float64
+    @show typeof(grad)
+
+    # TODO: This is currently broken –
+    # @test grad isa Vector{Float64}
+    # @test val isa Float64
+    # @test typeof(grad.tree) <: DynamicExpressions.ChainRulesModule.NodeTangent{Float64, Node{Float64}, Vector{Float64}}
+    # @test typeof(grad.tree.gradient) <: Vector{Float64}
+end
