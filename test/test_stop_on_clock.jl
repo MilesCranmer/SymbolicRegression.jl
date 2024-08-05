33bd4fe2104b68cf26e6f8b643cf36eebbfeea19
MilesCranmer <miles.cranmer@gmail.com>
2024-08-05 07:26:01 +0100

    test: make clocked test use serial mode

--- test/test_stop_on_clock.jl
+++ test/test_stop_on_clock.jl
@@ -7,9 +7,9 @@ y = 2 * cos.(X[4, :])
 
 # Ensure is precompiled:
 options = Options(; default_params..., timeout_in_seconds=1)
-equation_search(X, y; niterations=1, options=options, parallelism=:multithreading)
+equation_search(X, y; niterations=1, options=options, parallelism=:serial)
 
 start_time = time()
-equation_search(X, y; niterations=10000000, options=options, parallelism=:multithreading)
+equation_search(X, y; niterations=10000000, options=options, parallelism=:serial)
 end_time = time()
 @test end_time - start_time < 100
