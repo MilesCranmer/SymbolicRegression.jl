16582d3c152567efbad45c8c883a17b30b517f68
MilesCranmer <miles.cranmer@gmail.com>
2024-08-12 23:25:07 +0100

    test: ensure options are lighterweight for clock test

--- test/test_stop_on_clock.jl
+++ test/test_stop_on_clock.jl
@@ -1,14 +1,24 @@
 using SymbolicRegression
 using Random
+using Distributed: rmprocs
 include("test_params.jl")
 
 X = randn(MersenneTwister(0), Float32, 5, 100)
 y = 2 * cos.(X[4, :])
 
 # Ensure is precompiled:
-options = Options(; default_params..., timeout_in_seconds=1)
+options = Options(;
+    default_params...,
+    population_size=10,
+    ncycles_per_iteration=100,
+    maxsize=15,
+    timeout_in_seconds=1,
+)
 equation_search(X, y; niterations=1, options=options, parallelism=:serial)
 
+# Ensure nothing might prevent slow checking of the clock:
+rmprocs()
+GC.gc(true) # full=true
 start_time = time()
 equation_search(X, y; niterations=10000000, options=options, parallelism=:serial)
 end_time = time()
