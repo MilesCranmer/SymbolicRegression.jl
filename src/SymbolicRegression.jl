e5c342748b33bf9b28f2b65deb01cebd5ff21e9e
Miles Cranmer <miles.cranmer@gmail.com>
2024-10-14 09:34:51 +0900

    fix: re-enable precompilation

--- src/SymbolicRegression.jl
+++ src/SymbolicRegression.jl
@@ -1100,7 +1100,7 @@ using ConstructionBase: ConstructionBase as _
 include("precompile.jl")
 redirect_stdout(devnull) do
     redirect_stderr(devnull) do
-        # do_precompilation(Val(:precompile))
+        do_precompilation(Val(:precompile))
     end
 end
 
