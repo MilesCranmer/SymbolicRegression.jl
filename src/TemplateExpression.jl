436ff2390c00a89132fae180ba98406f6291b4a5
MilesCranmer <miles.cranmer@gmail.com>
2024-11-03 14:18:38 +0000

    docs: update docs for TemplateStructure

--- src/TemplateExpression.jl
+++ src/TemplateExpression.jl
@@ -40,7 +40,7 @@ using ..PopMemberModule: PopMember
 using ..ComposableExpressionModule: ComposableExpression, ValidVector
 
 """
-    TemplateStructure{K,S,N,E,C} <: Function
+    TemplateStructure{K,E,NF} <: Function
 
 A struct that defines a prescribed structure for a `TemplateExpression`,
 including functions that define the result in different contexts.
@@ -50,11 +50,11 @@ If not declared using the constructor `TemplateStructure{K}(...)`, the keys of t
 `variable_constraints` `NamedTuple` will be used to infer this.
 
 # Fields
-- `combine`: Required function taking a `NamedTuple` of callable expressions (with keys `K`),
-    and a tuple representing the data. For example, `((; f, g), (x1, x2, x3)) -> f(x1, x2) + g(x3)`
-    would be a valid `combine` function. You may also re-use the callable expressions and
-    use different inputs, such as `((; f, g), (x1, x2)) -> f(x1 + g(x2)) - g(x1)` is
-    another valid choice.
+- `combine`: Required function taking a `NamedTuple` of `ComposableExpression`s (sharing the keys `K`),
+    and then tuple representing the data of `ValidVector`s. For example,
+    `((; f, g), (x1, x2, x3)) -> f(x1, x2) + g(x3)` would be a valid `combine` function. You may also
+    re-use the callable expressions and use different inputs, such as
+    `((; f, g), (x1, x2)) -> f(x1 + g(x2)) - g(x1)` is another valid choice.
 - `num_features`: Optional `NamedTuple` of function keys => integers representing the number of
     features used by each expression. If not provided, it will be inferred using the `combine`
     function. For example, if `f` takes two arguments, and `g` takes one, then
