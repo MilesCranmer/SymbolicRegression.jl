ffd290038ab27660da3689754f79815050e0146a
Jerry Ling <proton@jling.dev>
2025-04-25 11:17:42 -0400

    Unused `TemplateExpression` in template_expression_complex.jl
    ```julia
    ex = TemplateExpression(
        (; B_x, B_y, B_z, F_d_scale);
        structure, operators, variable_names
    )
    ```
    
    is un-used, should it be used?

--- examples/template_expression_complex.jl
+++ examples/template_expression_complex.jl
@@ -257,7 +257,7 @@ model = SRRegressor(;
     unary_operators=(sin, cos, sqrt, exp),
     niterations=500,
     maxsize=35,
-    expression_spec=TemplateExpressionSpec(; structure),
+    expression_spec=ex,
     ## Note that the elementwise loss needs to operate directly on each row of `y`:
     elementwise_loss=(F1, F2) -> (F1.x - F2.x)^2 + (F1.y - F2.y)^2 + (F1.z - F2.z)^2,
     batching=true,
