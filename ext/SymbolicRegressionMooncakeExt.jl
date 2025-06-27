882b454412f6e22fe3f103f774bab71e14835da8
MilesCranmer <miles.cranmer@gmail.com>
2025-06-27 18:33:38 +0100

    feat: add back missing tangent type

--- ext/SymbolicRegressionMooncakeExt.jl
+++ ext/SymbolicRegressionMooncakeExt.jl
@@ -4,17 +4,32 @@ using DynamicExpressions: DynamicExpressions as DE
 using SymbolicRegression: SymbolicRegression as SR
 using Mooncake: Mooncake
 
+if !applicable(Mooncake.tangent_type, Union{Mooncake.NoFData,Float64}, Mooncake.NoRData)
+    @eval function Mooncake.tangent_type(
+        ::Type{U}, ::Type{Mooncake.NoRData}
+    ) where {U<:Union{Mooncake.NoFData,T} where {T}}
+        return Mooncake.tangent_type(T)
+    end
+end
+
 function DE.extract_gradient(
     gradient::Mooncake.Tangent, ex::SR.TemplateExpression{T}
 ) where {T}
-    arrays = Vector{T}[]
+    n_const = DE.count_scalar_constants(ex)
+    out = Array{T}(undef, n_const)
+    i = firstindex(out)
     for (tree_gradient, tree) in zip(values(gradient.fields.trees), values(ex.trees))
         if !(tree_gradient isa Mooncake.NoTangent)
-            push!(arrays, DE.extract_gradient(tree_gradient, tree))
+            grad_array = DE.extract_gradient(tree_gradient, tree)
+            @inbounds for g in grad_array
+                i = DE.pack_scalar_constants!(out, i, g)
+            end
         else
             num_constants = DE.count_scalar_constants(tree)
             if num_constants > 0
-                push!(arrays, zeros(T, num_constants))
+                @inbounds for _ in 1:num_constants
+                    i = DE.pack_scalar_constants!(out, i, zero(T))
+                end
             end
         end
     end
@@ -24,13 +39,16 @@ function DE.extract_gradient(
             values(ex.metadata.parameters),
         )
             if !(param_gradient isa Mooncake.NoTangent)
-                push!(arrays, param_gradient.fields._data)
+                @inbounds for g in param_gradient.fields._data
+                    i = DE.pack_scalar_constants!(out, i, g)
+                end
             else
-                push!(arrays, zeros(T, length(param)))
+                @inbounds for _ in 1:length(param)
+                    i = DE.pack_scalar_constants!(out, i, zero(T))
+                end
             end
         end
     end
-    out = vcat(arrays...)
     return out
 end
 function DE.extract_gradient(gradient::Mooncake.Tangent, ex::SR.ComposableExpression)
