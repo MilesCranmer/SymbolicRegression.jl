3654e6aea6f623aec2ce55fa4e73c4e2752951af
MilesCranmer <miles.cranmer@gmail.com>
2025-07-19 15:09:32 +0200

    cleaner handling of extract gradient

--- ext/SymbolicRegressionMooncakeExt.jl
+++ ext/SymbolicRegressionMooncakeExt.jl
@@ -12,18 +12,13 @@ function DE.extract_gradient(
     out = Array{T}(undef, n_const)
     i = firstindex(out)
     for (tree_gradient, tree) in zip(values(gradient.fields.trees), values(ex.trees))
-        if !(tree_gradient isa Mooncake.NoTangent)
-            grad_array = DE.extract_gradient(tree_gradient, tree)
-            @inbounds for g in grad_array
-                i = DE.pack_scalar_constants!(out, i, g)
-            end
-        else
-            num_constants = count_constants_for_optimization(tree)
-            if num_constants > 0
-                @inbounds for _ in 1:num_constants
-                    i = DE.pack_scalar_constants!(out, i, zero(T))
-                end
-            end
+        @assert(
+            !(tree_gradient isa Mooncake.NoTangent),
+            "Unexpected input type: $(tree_gradient)::$(typeof(tree_gradient))"
+        )
+        grad_array = DE.extract_gradient(tree_gradient, tree)
+        @inbounds for g in grad_array
+            i = DE.pack_scalar_constants!(out, i, g)
         end
     end
     if SR.has_params(ex)
@@ -31,14 +26,12 @@ function DE.extract_gradient(
             values(gradient.fields.metadata.fields._data.parameters),
             values(ex.metadata.parameters),
         )
-            if !(param_gradient isa Mooncake.NoTangent)
-                @inbounds for g in param_gradient.fields._data
-                    i = DE.pack_scalar_constants!(out, i, g)
-                end
-            else
-                @inbounds for _ in 1:length(param)
-                    i = DE.pack_scalar_constants!(out, i, zero(T))
-                end
+            @assert(
+                !(param_gradient isa Mooncake.NoTangent),
+                "Unexpected input type: $(param_gradient)::$(typeof(param_gradient))"
+            )
+            @inbounds for g in param_gradient.fields._data
+                i = DE.pack_scalar_constants!(out, i, g)
             end
         end
     end
