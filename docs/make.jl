9aeb34c2fc7b0de8259362a26a6b68029a71d75e
MilesCranmer <miles.cranmer@gmail.com>
2025-10-11 23:10:11 +0100

    hotfix(docs): fix `/dev/` link

--- docs/make.jl
+++ docs/make.jl
@@ -185,9 +185,9 @@ function fix_vitepress_base_path()
 
     # Determine the correct base path for each deployment
     base_path = if deployment_target == "cambridge"
-        "/symbolicregression/"
+        "/symbolicregression/dev/"
     else
-        "/SymbolicRegression.jl/"
+        "/SymbolicRegression.jl/dev/"
     end
 
     # Find and fix VitePress SOURCE config file (before build)
